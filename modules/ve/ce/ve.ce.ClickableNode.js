d9458eb9f55c93b3617c330ca085d2efea7dc6a6
Rob Moen <rmoen@wikimedia.org>
2013-12-20 15:17:15 -0800

    Create mixin for node that responds to node click events
    Currently this mixin responds to double clicks by trying to open
    a dialog for the tool associated with the focused node.
    
    Method could be enhanced to support inspectors, but at the moment
    I believe nodes are only related to dialogs.
    
    Bug: 50996
    Change-Id: I8df07f413d9e295b960b7d0c82224aa37fdd637e

---
+++ modules/ve/ce/ve.ce.ClickableNode.js
@@ -0,0 +1,39 @@
+/*!
+ * VisualEditor ContentEditable ClickableNode class.
+ *
+ * @copyright 2011-2013 VisualEditor Team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+
+/**
+ * ContentEditable clickable node.
+ * Whatever node this is being mixed into must emit a dblclick event
+ *
+ * @class
+ * @abstract
+ *
+ * @constructor
+ */
+ve.ce.ClickableNode = function VeCeClickableNode() {
+	// Events
+	this.connect( this, { 'dblclick': 'onDblClick' } );
+
+};
+/**
+ * Finds and opens the tool for the double clicked node
+ *
+ */
+ve.ce.ClickableNode.prototype.onDblClick = function () {
+	var ceSurface = this.getRoot().getSurface(),
+		uiSurface = ceSurface.getSurface(),
+		name;
+
+	name = ve.ui.toolFactory.getToolForNode(
+		ceSurface.getFocusedNode().getModel()
+	);
+	if ( ve.ui.dialogFactory.lookup( name ) ) {
+		uiSurface.execute( 'dialog', 'open', name );
+	} else if ( ve.ui.inspectorFactory.lookup( name ) ) {
+		uiSurface.exectue( 'inspector', 'open', name );
+	}
+};
