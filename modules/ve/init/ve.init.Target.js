0575dac722c7bf3d91e00ed3ea612d5befae9d0e
Ed Sanders <esanders@wikimedia.org>
2014-01-25 23:33:46 +0000

    Attach container to DOM before initialising target in demo
    Otherwise toolbar measurements fail and it collapses
    
    Bug: 59720
    Change-Id: Ia296f073b40abe4a5193927b8da960a5d54c99df

--- modules/ve/init/ve.init.Target.js
+++ modules/ve/init/ve.init.Target.js
@@ -13,7 +13,7 @@
  * @mixins OO.EventEmitter
  *
  * @constructor
- * @param {jQuery} $container Conainter to render target into
+ * @param {jQuery} $container Conainter to render target into, must be attached to the DOM
  */
 ve.init.Target = function VeInitTarget( $container ) {
 	// Mixin constructors
