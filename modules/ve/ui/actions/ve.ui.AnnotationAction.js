94e2a57eee2602cee665e6b08ce22f19510af6a6
Ed Sanders <esanders@wikimedia.org>
2014-02-20 16:12:29 +0000

    Allow annotations to remove other annotations
    In this case, superscript should remove subscript, and vice-versa.
    
    Bug: 53330
    Change-Id: I5abaacd7389c6040b9906ddb61e46927754a41d6

--- modules/ve/ui/actions/ve.ui.AnnotationAction.js
+++ modules/ve/ui/actions/ve.ui.AnnotationAction.js
@@ -69,19 +69,33 @@ ve.ui.AnnotationAction.prototype.clear = function ( name, data ) {
  * @param {Object} [data] Additional annotation data
  */
 ve.ui.AnnotationAction.prototype.toggle = function ( name, data ) {
-	var existingAnnotations,
+	var i, existingAnnotations, insertionAnnotations, removesAnnotations,
 		surfaceModel = this.surface.getModel(),
 		fragment = surfaceModel.getFragment(),
-		annotation = ve.dm.annotationFactory.create( name, data );
+		annotation = ve.dm.annotationFactory.create( name, data ),
+		removes = annotation.constructor.static.removes;
 
 	if ( !fragment.getRange().isCollapsed() ) {
-		fragment.annotateContent(
-			fragment.getAnnotations().containsComparable( annotation ) ? 'clear' : 'set', name, data
-		);
+		if ( !fragment.getAnnotations().containsComparable( annotation ) ) {
+			if ( removes ) {
+				for ( i = removes.length - 1; i >= 0; i-- ) {
+					fragment.annotateContent( 'clear', removes[i] );
+				}
+			}
+			fragment.annotateContent( 'set', name, data );
+		} else {
+			fragment.annotateContent( 'clear', name );
+		}
 	} else {
-		existingAnnotations = surfaceModel
-			.getInsertionAnnotations().getAnnotationsByName( annotation.name );
+		insertionAnnotations = surfaceModel.getInsertionAnnotations();
+		existingAnnotations = insertionAnnotations.getAnnotationsByName( annotation.name );
 		if ( existingAnnotations.isEmpty() ) {
+			if ( removes ) {
+				removesAnnotations = insertionAnnotations.filter( function ( annotation ) {
+					return ve.indexOf( annotation.name, removes ) !== -1;
+				} );
+				surfaceModel.removeInsertionAnnotations( removesAnnotations );
+			}
 			surfaceModel.addInsertionAnnotations( annotation );
 		} else {
 			surfaceModel.removeInsertionAnnotations( existingAnnotations );
