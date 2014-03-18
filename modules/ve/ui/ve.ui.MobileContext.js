fe1b7c3d6a82b3c0b952a9e406a099712615e271
Juliusz Gonera <jgonera@wikimedia.org>
2014-03-18 16:33:17 -0700

    [WIP] Style link inspector on mobile
    Change-Id: I32d94337a95db9a492b0c06015b482ec21176867

--- modules/ve/ui/ve.ui.MobileContext.js
+++ modules/ve/ui/ve.ui.MobileContext.js
@@ -44,17 +44,19 @@ OO.inheritClass( ve.ui.MobileContext, ve.ui.Context );
  * @chainable
  */
 ve.ui.MobileContext.prototype.show = function () {
-	this.$element.addClass( 've-ui-mobileContext-visible' );
-	this.scrollPos = $( 'body' ).scrollTop();
 	// overflow: hidden on 'body' alone is not enough for iOS Safari
 	$( 'html, body' ).addClass( 've-ui-mobileContext-enabled' );
+	this.$element.addClass( 've-ui-mobileContext-visible' );
+	this.scrollPos = $( 'body' ).scrollTop();
 };
 
 /**
  * @inheritdoc
  */
 ve.ui.MobileContext.prototype.hide = function () {
-	$( 'html, body' ).removeClass( 've-ui-mobileContext-enabled' );
 	$( 'body' ).scrollTop( this.scrollPos );
 	this.$element.removeClass( 've-ui-mobileContext-visible' );
+	setTimeout( function() {
+		$( 'html, body' ).removeClass( 've-ui-mobileContext-enabled' );
+	}, 300 );
 };
