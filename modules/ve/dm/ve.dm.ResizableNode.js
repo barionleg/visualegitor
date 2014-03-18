af28da461be790963bc0619c614b3abc48610b34
Moriel Schottlender <moriel@gmail.com>
2014-03-17 19:01:56 -0700

    [WIP] Refactor Scalable functionality
    This fix transformed scalable into a calculation engine that is
    delivered by resizable objects. The scalable object is responsible
    for all calculations and adjustments that relate to size for the
    given node.
    
    As a result, everything that relates to size calculation has been
    touched.
    
    On top of that, media size widget has been adjusted to work with
    scalable object and also display the dimensions through the
    dimensions widget.
    
    This change is completely dependent on
    change I017a017924f544cc8bc9b7d8245335759ae0e890
    
    Change-Id: Ie7ca3bbbe5d0287c7ce17d296c1827b8213ea950

---
+++ modules/ve/dm/ve.dm.ResizableNode.js
@@ -0,0 +1,46 @@
+/*!
+ * VisualEditor DataModel Resizable node.
+ *
+ * @copyright 2011-2014 VisualEditor Team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+
+/**
+ * A mixin class for resizable nodes. This class is mostly a base
+ * interface for resizable nodes to be able to produce scalable
+ * objects for further calculation.
+ *
+ * @class
+ * @abstract
+ * @constructor
+ */
+ve.dm.ResizableNode = function VeDmResizableNode( config ) {
+	config = config || {};
+};
+
+/**
+ * Produce a scalable object based on the current object's width
+ * and height properties. This should be implemented in the
+ * specific instances of resizable nodes; the basic operation
+ * assumes at least current width and height.
+ *
+ * @returns {jQuery.Promise} Promise that resolves to a
+ *  ve.dm.Scalable object
+ */
+ve.dm.ResizableNode.prototype.getScalable = function () {
+	throw new Error( 've.dm.ResizableNode subclass must implement getScalable' );
+};
+
+/**
+ * Produce a basic, non asynchronous scalable object based on the
+ * current object's width and height properties.
+ *
+ * This should be implemented in the specific instances of
+ * resizable nodes; the basic operation assumes at least current
+ * width and height.
+ *
+ * @returns {ve.dm.Scalable}
+ */
+ve.dm.ResizableNode.prototype.getBasicScalable = function () {
+	throw new Error( 've.dm.ResizableNode subclass must implement getBasicScalable' );
+};
