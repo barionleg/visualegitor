70b296506bdabdfe69404150d2847c8f23f817dd
Ed Sanders <esanders@wikimedia.org>
2014-05-12 23:26:15 +0200

    Stage slugs
    As soon the cursor is placed in a slug, stage the creation of
    a real paragraph.
    
    If the the cursor is moved out of the slug and it is still empty
    then pop the staging stack.
    
    If the staged paragraph is added to, commit the staging stack.
    
    As part of this slugs are now CE=false to reflect the fact they
    don't exist in the model until the are clicked (not just selected
    over). This avoids strange behaviour with selections that only
    start xor end in a slug.
    
    Future to-do:
    * Fix or kill animations as this breaks them
    * Possibly:
      + Fix existing bug with left/right arrowing into a slug
      + Fix up/down arrowing into a slug now they're CE=false
    * Kill getSlugAtOffset, and any other code which detects for the
      presence of a block slug.
    
    Bug: 65052
    Change-Id: Ic857215654eaf5ee32e19d7db1d83185f561f783

--- src/ce/ve.ce.Surface.js
+++ src/ce/ve.ce.Surface.js
@@ -66,7 +66,11 @@ ve.ce.Surface = function VeCeSurface( model, surface, options ) {
 
 	// Events
 	this.surfaceObserver.connect(
-		this, { contentChange: 'onContentChange', selectionChange: 'onSelectionChange' }
+		this, {
+			contentChange: 'onContentChange',
+			selectionChange: 'onSelectionChange',
+			slugChange: 'onSlugChange'
+		}
 	);
 	this.model.connect( this,
 		{ select: 'onModelSelect', documentUpdate: 'onModelDocumentUpdate' }
@@ -800,7 +804,7 @@ ve.ce.Surface.prototype.onDocumentKeyPress = function ( e ) {
 
 /**
  * Poll again after the native key press
- * @param {jQuery.Event} ev
+ * @param {jQuery.Event} e Key press event
  */
 ve.ce.Surface.prototype.afterDocumentKeyPress = function () {
 	this.surfaceObserver.pollOnce();
@@ -1305,6 +1309,10 @@ ve.ce.Surface.prototype.afterPaste = function () {
 	this.model.change( tx, new ve.Range( selection.start ) );
 	// Move cursor to end of selection
 	this.model.setSelection( new ve.Range( selection.end ) );
+
+	// Make sure the surface observer emits slugChange events
+	this.surfaceObserver.clear();
+	this.surfaceObserver.pollOnce();
 };
 
 /**
@@ -1414,6 +1422,7 @@ ve.ce.Surface.prototype.onModelSelect = function ( selection ) {
 
 /**
  * Handle documentUpdate events on the surface model.
+ *
  * @param {ve.dm.Transaction} tx
  * @param {boolean} staged
  */
@@ -1449,6 +1458,52 @@ ve.ce.Surface.prototype.onSelectionChange = function ( oldRange, newRange ) {
 		this.changeModel( null, newRange );
 	} finally {
 		this.decRenderLock();
+	}
+
+};
+
+/**
+ * Handle slug change events.
+ *
+ * @see ve.ce.SurfaceObserver#pollOnce
+ *
+ * @param {ve.Range|null} range
+ * @param {boolean} newSlug
+ */
+ve.ce.Surface.prototype.onSlugChange = function ( range, newSlug ) {
+	if ( this.slugFragment || newSlug ) {
+		var model = this.getModel(),
+			doc = model.getDocument(),
+			fragment = model.getFragment( range );
+
+		if ( this.slugFragment ) {
+			if ( this.slugFragment.getRange().getLength() === 2 ) {
+				if ( !range || !this.slugFragment.getRange().containsOffset( range.start ) ) {
+					model.popStaging();
+					// After popStaging we may have removed a paragraph before our current
+					// cursor position. Polling with the SurfaceObserver won't notice a change
+					// in the rangy range as our cursor doesn't move within its node so we
+					// need to clear it first.
+					this.surfaceObserver.clear();
+					this.surfaceObserver.pollOnceNoEmit();
+					this.slugFragment = null;
+				}
+			} else {
+				model.applyStaging();
+				this.slugFragment = null;
+			}
+		}
+		if ( newSlug ) {
+			range = fragment.getRange();
+			model.pushStaging( true );
+			this.changeModel( ve.dm.Transaction.newFromInsertion(
+				doc, range.start, [
+					{ type: 'paragraph', internal: { generated: 'slug' } },
+					{ type: '/paragraph' }
+				]
+			), new ve.Range( range.start + 1 ) );
+			this.slugFragment = fragment;
+		}
 	}
 };
 
