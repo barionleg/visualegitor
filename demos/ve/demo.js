89adf612e94f45b9c288a242415743520ebe1fd7
Ed Sanders <esanders@wikimedia.org>
2014-06-30 21:54:53 +0100

    Widgetise demo menu
    Also separate out menu functionality from the debug bar.
    
    Change-Id: I04c1929c5279968ca3b0ec475681bd587ae58356

--- demos/ve/demo.js
+++ demos/ve/demo.js
@@ -7,12 +7,121 @@
 
 $( function () {
 
+	function getDemoPageItems () {
+		var name, items = [];
+		for ( name in ve.demoPages ) {
+			items.push(
+				new OO.ui.MenuItemWidget( ve.demoPages[name],  { 'label': name } )
+			);
+		}
+		return items;
+	}
+
 	var currentTarget,
 		initialPage,
 
-		$targetContainer = $( '.ve-demo-editor' ).eq( 0 ),
+		$menu = $( '.ve-demo-menu' ),
+		$targetContainer = $( '.ve-demo-editor' ),
 		lang = $.i18n().locale,
-		dir = $targetContainer.css( 'direction' ) || 'ltr';
+		dir = $targetContainer.css( 'direction' ) || 'ltr',
+
+		// Menu widgets
+		pageMenu = new OO.ui.InlineMenuWidget( {
+			'menu': {
+				'items': getDemoPageItems()
+			}
+		} ),
+		pageLabel = new OO.ui.LabelWidget(
+			{ 'label': 'Page', 'input': pageMenu }
+		),
+
+		convertButton = new OO.ui.ButtonWidget( { '$': this.$, 'label': 'Edit HTML' } ),
+		languageTextInput = new OO.ui.TextInputWidget( { '$': this.$, 'value': lang } ),
+		languageDirectionButton = new OO.ui.ButtonWidget( { '$': this.$, 'label': 'Set language & direction' } ),
+		directionSelect = new OO.ui.ButtonSelectWidget().addItems( [
+			new OO.ui.ButtonOptionWidget( 'rtl', { '$': this.$, 'icon': 'text-dir-rtl' } ),
+			new OO.ui.ButtonOptionWidget( 'ltr', { '$': this.$, 'icon': 'text-dir-ltr' } )
+		] ),
+		sourceMode = false,
+		sourceTextInput = new OO.ui.TextInputWidget( {
+			'$': this.$,
+			'multiline': true,
+			'autosize': true,
+			'maxRows': 999,
+			'classes': ['ve-demo-source']
+		} );
+
+	// Initialization
+	pageMenu.getMenu().on( 'select', function ( item ) {
+		var page = item.getData();
+		if ( window.history.replaceState ) {
+			window.history.replaceState( null, document.title, '#!/src/' + page );
+		}
+		loadPage( page );
+	} );
+
+	languageDirectionButton.on( 'click', function () {
+		$.i18n().locale = lang = languageTextInput.getValue();
+		dir = directionSelect.getSelectedItem().getData();
+
+		// HACK: Override/restore message functions for qqx mode
+		if ( lang === 'qqx' ) {
+			ve.init.platform.getMessage = function ( key ) { return key; };
+		} else {
+			ve.init.platform.getMessage = ve.init.sa.Platform.prototype.getMessage;
+		}
+
+		// Re-bind as getMessage may have changed
+		OO.ui.msg = ve.bind( ve.init.platform.getMessage, ve.init.platform );
+
+		// HACK: Re-initialize page to load message files
+		ve.init.platform.initialize().done( function () {
+			loadPage( location.hash.slice( 7 ), true );
+		} );
+	} );
+
+	convertButton.on( 'click', function () {
+		var doc, html, $documentNode,
+			surfaceView = currentTarget.getSurface().getView();
+
+		sourceMode = !sourceMode;
+		if ( sourceMode ) {
+			doc = ve.dm.converter.getDomFromModel( currentTarget.getSurface().getModel().getDocument() );
+			html = ve.properInnerHtml( doc.body );
+			$documentNode = surfaceView.getDocument().getDocumentNode().$element;
+
+			sourceTextInput.$element.show();
+			sourceTextInput.setValue( html ).adjustSize();
+			sourceTextInput.$element.hide();
+
+			$documentNode.slideUp().promise().done( function () {
+				convertButton.setLabel( 'VisualEditor' );
+				sourceTextInput.$element.slideDown().promise().done( function () {
+					sourceTextInput.focus();
+				} );
+				if ( ve.debug ) {
+					currentTarget.debugBar.$element.remove();
+				}
+			} );
+		} else {
+			loadTarget( sourceTextInput.getValue() );
+		}
+	} );
+
+	directionSelect.selectItem( directionSelect.getItemFromData( dir ) );
+
+	$menu.append(
+		$( '<div>' ).addClass( 've-ui-debugBar-commands' ).append(
+			pageLabel.$element,
+			pageMenu.$element,
+			$( ve.ui.DebugBar.static.dividerTemplate ),
+			convertButton.$element,
+			$( ve.ui.DebugBar.static.dividerTemplate ),
+			languageTextInput.$element,
+			directionSelect.$element,
+			languageDirectionButton.$element
+		)
+	);
 
 	/**
 	 * Load a page into the editor
@@ -25,6 +134,7 @@ $( function () {
 		if ( !forceDir ) {
 			dir = src.match( /rtl\.html$/ ) ? 'rtl' : 'ltr';
 		}
+
 		$.ajax( {
 			url: src,
 			dataType: 'text'
@@ -47,6 +157,8 @@ $( function () {
 			if ( currentTarget ) {
 				currentTarget.destroy();
 			}
+
+			convertButton.setLabel( 'Edit HTML' );
 
 			// Container needs to be visually hidden, but not display:none
 			// so that the toolbar can be measured
@@ -81,108 +193,24 @@ $( function () {
 
 				$targetContainer.slideDown().promise().done( function () {
 					surfaceView.focus();
-					if ( ve.debug ) {
-						// Widgets
-						var convertButton = new OO.ui.ButtonWidget( { '$': this.$, 'label': 'Edit HTML' } ),
-							languageTextInput = new OO.ui.TextInputWidget( { '$': this.$, 'value': lang } ),
-							languageDirectionButton = new OO.ui.ButtonWidget( { '$': this.$, 'label': 'Set language & direction' } ),
-							directionSelect = new OO.ui.ButtonSelectWidget().addItems( [
-								new OO.ui.ButtonOptionWidget( 'rtl', { '$': this.$, 'icon': 'text-dir-rtl' } ),
-								new OO.ui.ButtonOptionWidget( 'ltr', { '$': this.$, 'icon': 'text-dir-ltr' } )
-							] ),
-							sourceMode = false,
-							sourceTextInput = new OO.ui.TextInputWidget( {
-								'$': this.$,
-								'multiline': true,
-								'autosize': true,
-								'maxRows': 999,
-								'classes': ['ve-demo-source']
-							} );
-
-						directionSelect.selectItem( directionSelect.getItemFromData( dir ) );
-						surfaceView.$element.append( sourceTextInput.$element.hide() );
-
-						// Initialization
-						languageDirectionButton.on( 'click', function () {
-							$.i18n().locale = lang = languageTextInput.getValue();
-							dir = directionSelect.getSelectedItem().getData();
-
-							// HACK: Override/restore message functions for qqx mode
-							if ( lang === 'qqx' ) {
-								ve.init.platform.getMessage = function ( key ) { return key; };
-							} else {
-								ve.init.platform.getMessage = ve.init.sa.Platform.prototype.getMessage;
-							}
-
-							// Re-bind as getMessage may have changed
-							OO.ui.msg = ve.bind( ve.init.platform.getMessage, ve.init.platform );
-
-							// HACK: Re-initialize page to load message files
-							ve.init.platform.initialize().done( function () {
-								loadPage( location.hash.slice( 7 ), true );
-							} );
-						} );
-
-						convertButton.on( 'click', function () {
-							var doc, html, $documentNode;
-							sourceMode = !sourceMode;
-							if ( sourceMode ) {
-								doc = ve.dm.converter.getDomFromModel( currentTarget.getSurface().getModel().getDocument() );
-								html = ve.properInnerHtml( doc.body );
-								$documentNode = surfaceView.getDocument().getDocumentNode().$element;
-
-								sourceTextInput.$element.show();
-								sourceTextInput.setValue( html ).adjustSize();
-								sourceTextInput.$element.hide();
-
-								$documentNode.slideUp().promise().done( function () {
-									convertButton.setLabel( 'VisualEditor' );
-									sourceTextInput.$element.slideDown().promise().done( function () {
-										sourceTextInput.focus();
-									} );
-									currentTarget.debugBar.dumpModelButton.setDisabled( true );
-									currentTarget.debugBar.dumpModelChangeToggle.setDisabled( true );
-									languageTextInput.setDisabled( true );
-									directionSelect.setDisabled( true );
-									languageDirectionButton.setDisabled( true );
-								} );
-							} else {
-								loadTarget( sourceTextInput.getValue() );
-							}
-						} );
-
-						directionSelect.selectItem( directionSelect.getItemFromData( dir ) );
-
-						currentTarget.debugBar.$commands.append(
-							$( ve.ui.DebugBar.static.dividerTemplate ),
-							convertButton.$element,
-							$( ve.ui.DebugBar.static.dividerTemplate ),
-							languageTextInput.$element,
-							directionSelect.$element,
-							languageDirectionButton.$element
-						);
-					}
+					surfaceView.$element.append( sourceTextInput.$element.hide() );
 				} );
 			} );
 		} );
 	}
 
 	// Open initial page
-
 	if ( /^#!\/src\/.+$/.test( location.hash ) ) {
-		loadPage( location.hash.slice( 7 ) );
+		initialPage = location.hash.slice( 7 );
 	} else {
-		initialPage = $( '.ve-demo-menu li a' ).data( 'pageSrc' );
-		if ( window.history.replaceState ) {
-			window.history.replaceState( null, document.title, '#!/src/' + initialPage );
-		}
+		initialPage = pageMenu.getMenu().getFirstSelectableItem().getData();
 		// Per W3 spec, history.replaceState does not fire hashchange
-		loadPage( initialPage );
 	}
+	pageMenu.getMenu().selectItem( pageMenu.getMenu().getItemFromData( initialPage ) );
 
 	window.addEventListener( 'hashchange', function () {
 		if ( /^#!\/src\/.+$/.test( location.hash ) ) {
-			loadPage( location.hash.slice( 7 ) );
+			pageMenu.getMenu().selectItem( pageMenu.getMenu().getItemFromData( location.hash.slice( 7 ) ) );
 		}
 	} );
 
