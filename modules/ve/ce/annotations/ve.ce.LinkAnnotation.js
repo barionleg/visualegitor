54c3249e3c926b4971a4ea75441529a2d492fbc6
Ed Sanders <esanders@wikimedia.org>
2014-06-30 21:56:36 +0100

    Prevent iOS Safari from following links
    This happens when the keyboard is closed. Upstream bug but broken
    in all major versions so we need a workaround.
    
    Bug: 65326
    Change-Id: I8a99fa7d7c19a3c3468959935726a21059a582c5

--- modules/ve/ce/annotations/ve.ce.LinkAnnotation.js
+++ modules/ve/ce/annotations/ve.ce.LinkAnnotation.js
@@ -20,8 +20,14 @@ ve.ce.LinkAnnotation = function VeCeLinkAnnotation( model, parentNode, config ) 
 	ve.ce.Annotation.call( this, model, parentNode, config );
 
 	// DOM changes
-	this.$element.addClass( 've-ce-LinkAnnotation' );
-	this.$element.attr( 'href', ve.resolveUrl( this.model.getHref(), this.getModelHtmlDocument() ) );
+	this.$element
+		.addClass( 've-ce-LinkAnnotation' )
+		.attr( 'href', ve.resolveUrl( this.model.getHref(), this.getModelHtmlDocument() ) )
+		// Some browsers will try to let links do their thing
+		// (e.g. iOS Safari when the keyboard is closed)
+		.on( 'click', function ( e ) {
+			e.preventDefault();
+		} );
 };
 
 /* Inheritance */
