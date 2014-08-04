5298fbe01b48045728bf2155a3acb962cb64603a
C. Scott Ananian <cscott@cscott.net>
2014-08-04 23:33:43 +0200

    Add ve.dm.Transaction.newNoOp() constructor
    Make it easier to create a no-op transaction (with appropriate
    retain/retainMetadata operations).
    
    Based on I77f19a2c532f4a47f51d2d8669c9d38c98ae8105
    
    Change-Id: I6e233d159f19b69f529ffd9c968b2fd5ee714292

--- src/dm/ve.dm.Transaction.js
+++ src/dm/ve.dm.Transaction.js
@@ -40,6 +40,18 @@ ve.dm.Transaction.coerceToCollapsedRange = function ( indexOrRange ) {
 };
 
 /**
+ * Generate a transaction that does nothing.
+ *
+ * @param {ve.dm.Document} doc Document to generate a transaction for
+ * @returns {ve.dm.Transaction}
+ */
+ve.dm.Transaction.newNoOp = function ( doc ) {
+	var tx = new ve.dm.Transaction( [ 'newNoOp' ] );
+	tx.pushFinalRetain( doc, 0 );
+	return tx;
+};
+
+/**
  * Generate a transaction that replaces data in a range.
  *
  * @method
