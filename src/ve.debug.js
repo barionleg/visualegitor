cd42e2341cfd8989c084473883ff8af088a1145f
divec <david@sheetmusic.org.uk>
2014-08-06 11:19:57 +0100

    ve.Filibuster: Scrupulous state logging
    ve.Filibuster
    * Wrap functions to log entry/exit
    * Log state and stack at each function call
    * Shortlist potentially significant state change observations
    
    ve.ui.DebugBar
    * Start Filibuster/Stop Filibuster button
    * Tabulated observation HTML
    
    ve.ui.Surface
    * ve.Filibuster hooks
    
    ve.ce.SurfaceObserver
    * Call setTimeout through a method, to facilitate disabling
    
    ve.EventSequencer
    * Perform all calls through a method 'callListener', for clearer logs
    
    ve.debug
    * Redefine OO.EventEmitter.prototype.connect to do late binding
    * define ve.error
    
    demos/*
    * Minor and consequential changes
    
    Change-Id: I559eba8bed17a7669b445d307c3866f2548f908c

--- src/ve.debug.js
+++ src/ve.debug.js
@@ -13,6 +13,42 @@
 ve.debug = true;
 
 /**
+ * Change the definition of "connect" so that it does late binding on method names.
+ *
+ * This is necessary so that ve.Filibuster can redefine functions after bindings have
+ * been applied. It's pretty horrible though, because it makes debug mode more different
+ * from production mode.
+ */
+OO.EventEmitter.prototype.connect = ( function ( connect ) {
+	return function ( context, methods ) {
+		var eventName, method,
+			eventEmitter = this,
+			wrappedMethods = {};
+
+		/**
+		 * Create a wrapper that looks up context[ method ] at call time
+		 */
+		function makeWrapper( method ) {
+			return function () {
+				return context[ method ].apply( context, arguments );
+			};
+		}
+
+		// bind names late
+		for ( eventName in methods ) {
+			method = methods[ eventName ];
+			if ( typeof method !== 'string' ) {
+				// the name is already bound; cannot wrap :-(
+				throw new Error( 'Early binding detected: ', context, method );
+			}
+			// bind name at call time
+			wrappedMethods[ eventName ] = makeWrapper( method );
+		}
+		return connect.call( eventEmitter, context, wrappedMethods );
+	};
+} ( OO.EventEmitter.prototype.connect ) );
+
+/**
  * @class ve.debug
  * @override ve
  * @singleton
@@ -31,6 +67,19 @@ ve.log = function () {
 	// from Function.prototype, thus console.log.apply does not exist.
 	// However it is function-like enough that passing it to Function#apply does work.
 	Function.prototype.apply.call( console.log, console, arguments );
+};
+
+/**
+ * Logs error to the console.
+ *
+ * @method
+ * @param {Mixed...} [data] Data to log
+ */
+ve.error = function () {
+	// In IE9 console methods are not real functions and as such do not inherit
+	// from Function.prototype, thus console.error.apply does not exist.
+	// However it is function-like enough that passing it to Function#apply does work.
+	Function.prototype.apply.call( console.error, console, arguments );
 };
 
 /**
