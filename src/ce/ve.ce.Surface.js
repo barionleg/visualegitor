ea29cf7e51dfc70e39c65029c013554a34804603
Oliver Buchtala <oliver.buchtala@gmail.com>
2014-09-09 17:08:59 +0200

    Provisional fix to prevent structural changes to a table via delete+merge
    Change-Id: I5229e55e0e83b29f1329f4c378caff77f2db0581

--- src/ce/ve.ce.Surface.js
+++ src/ce/ve.ce.Surface.js
@@ -2358,6 +2358,16 @@ ve.ce.Surface.prototype.handleDelete = function ( e ) {
 			if ( startNode.isFocusable() ) {
 				this.getModel().setLinearSelection( startNode.getOuterRange() );
 				return;
+			} else if ( !startNode.mergeOnDelete() ) {
+				// HACK: preventing a deletion of table nodes
+				// TODO: there are more cases which alter a tables structure, which must not happen:
+				// e.g., delete with non-collapsed selection, cut, paster
+				model.setSelection(
+					this.getDocument().getRelativeRange(
+						model.getSelection(), direction, 'character', false
+					)
+				);
+				return;
 			}
 		}
 		if ( rangeToRemove.isCollapsed() ) {
