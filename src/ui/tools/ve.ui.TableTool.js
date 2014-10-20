41468b44f52a3d0df3f02a93834652b2682e8ca3
Ed Sanders <esanders@wikimedia.org>
2014-10-20 18:03:52 +0100

    Merge cells tool
    Change-Id: If0ee115468c13412c52e5dba122105cdaca28e3e

--- src/ui/tools/ve.ui.TableTool.js
+++ src/ui/tools/ve.ui.TableTool.js
@@ -115,3 +115,17 @@ ve.ui.DeleteColumnTool.static.title =
 ve.ui.DeleteColumnTool.static.commandName = 'deleteColumn';
 ve.ui.DeleteColumnTool.static.requiresSelection = [ 'table' ];
 ve.ui.toolFactory.register( ve.ui.DeleteColumnTool );
+
+ve.ui.MergeCellsTool = function VeUiDeleteColumnTool( toolGroup, config ) {
+	ve.ui.Tool.call( this, toolGroup, config );
+};
+OO.inheritClass( ve.ui.MergeCellsTool, ve.ui.Tool );
+ve.ui.MergeCellsTool.static.name = 'mergeCells';
+ve.ui.MergeCellsTool.static.group = 'table';
+ve.ui.MergeCellsTool.static.autoAddToCatchall = false;
+ve.ui.MergeCellsTool.static.icon = 'table-merge-cells';
+ve.ui.MergeCellsTool.static.title =
+	OO.ui.deferMsg( 'visualeditor-table-merge-cells' );
+ve.ui.MergeCellsTool.static.commandName = 'mergeCells';
+ve.ui.MergeCellsTool.static.requiresSelection = [ 'table' ];
+ve.ui.toolFactory.register( ve.ui.MergeCellsTool );
