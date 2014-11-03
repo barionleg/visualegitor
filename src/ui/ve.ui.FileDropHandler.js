e1137103be761cdf272824e7fd9ed76f5681adc1
Ed Sanders <esanders@wikimedia.org>
2014-11-03 13:26:34 +0000

    Support for generic file drop handlers
    Change-Id: I1de3363a959d8297f73e650f2cc7fb6feb981d00

---
+++ src/ui/ve.ui.FileDropHandler.js
@@ -0,0 +1,73 @@
+/*!
+ * VisualEditor UserInterface file drop handler class.
+ *
+ * @copyright 2011-2014 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * File drop handler.
+ *
+ * @class
+ * @abstract
+ *
+ * @constructor
+ * @param {ve.ui.Surface} surface Surface
+ * @param {File} file File to handle
+ */
+ve.ui.FileDropHandler = function VeUiFileDropHandler( surface, file ) {
+	// Properties
+	this.surface = surface;
+	this.file = file;
+};
+
+/* Inheritance */
+
+OO.initClass( ve.ui.FileDropHandler );
+
+/* Static properties */
+
+/**
+ * Symbolic name for this handler. Must be unique.
+ *
+ * @static
+ * @property {string}
+ * @inheritable
+ */
+ve.ui.FileDropHandler.static.name = null;
+
+/**
+ * List of mime types supported by this handler
+ *
+ * @static
+ * @property {string[]}
+ * @inheritable
+ */
+ve.ui.FileDropHandler.static.types = [];
+
+/* Methods */
+
+/**
+ * Process the file
+ *
+ * @param {ve.dm.SurfaceFragment} fragment Fragment to insert at
+ */
+ve.ui.FileDropHandler.prototype.insert = function ( fragment ) {
+	fragment.insertContent( [
+		{ type: 'filePlaceholder', attributes: { name: this.file.name, size: this.file.size } },
+		{ type: '/filePlaceholder' }
+	] );
+	this.process().done( function ( data ) {
+		fragment.insertContent( data );
+	} ).fail( function () {
+		fragment.removeContent();
+	} );
+};
+
+/**
+ * Process the file
+ *
+ * @returns {jQuery.promise} A promise which resolves with the data to insert
+ */
+ve.ui.FileDropHandler.prototype.process = function () {
+	throw new Error( 've.ui.FileDropHandler subclass must implement process' );
+};
