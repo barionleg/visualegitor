4681a49ed87fd7646e4ccd7e1adabdfc3ef92245
Ed Sanders <esanders@wikimedia.org>
2015-01-06 21:27:12 +0000

    Fix demo dist build again
    cssUrlEmbed doesn't concat if given a list of files so it must
    come after conct. This means a few files don't get embedded as they
    have a different relative path, but these images were already broken
    in dist and the paths should be fixed separately.
    
    We can now run janus before embed safely and thus load rtl icons
    where required.
    
    Change-Id: Icb704c15404f29fdc47fb277e3ee931e454e2d87

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,9 +45,16 @@ module.exports = function ( grunt ) {
 			dist: [ 'dist/*', 'test-coverage/*' ]
 		},
 		cssUrlEmbed: {
+			options: {
+				// TODO: A few image paths aren't relative to src/ui/styles
+				failOnMissingUrl: false,
+				baseDir: 'src/ui/styles'
+			},
 			dist: {
-				dest: 'dist/visualEditor.css',
-				src: coreBuildFiles.styles
+				files: {
+					'dist/visualEditor.css': 'dist/visualEditor.css',
+					'dist/visualEditor.rtl.css': 'dist/visualEditor.rtl.css'
+				}
 			}
 		},
 		concat: {
@@ -60,13 +67,13 @@ module.exports = function ( grunt ) {
 			},
 			css: {
 				dest: 'dist/visualEditor.css',
-				src: ['dist/visualEditor.css']
+				src: coreBuildFiles.styles
 			}
 		},
 		cssjanus: {
 			dist: {
-				src: 'dist/visualEditor.css',
-				dest: 'dist/visualEditor.rtl.css'
+				dest: 'dist/visualEditor.rtl.css',
+				src: 'dist/visualEditor.css'
 			}
 		},
 		copy: {
@@ -205,7 +212,7 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'build', [ 'clean', 'cssUrlEmbed', 'concat', 'cssjanus', 'copy', 'buildloader' ] );
+	grunt.registerTask( 'build', [ 'clean', 'concat', 'cssjanus', 'cssUrlEmbed', 'copy', 'buildloader' ] );
 	grunt.registerTask( 'lint', [ 'jshint', 'jscs', 'csslint', 'banana' ] );
 	grunt.registerTask( 'unit', [ 'karma:main' ] );
 	grunt.registerTask( 'test', [ 'git-build', 'build', 'lint', 'unit' ] );
