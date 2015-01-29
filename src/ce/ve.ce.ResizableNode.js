79c2d930f4e20a56b5e9890c563432ebac379e1f
Moriel Schottlender <moriel@gmail.com>
2015-01-29 15:38:56 -0800

    Use css class rather than jQuery show/hide
    * Dependent on Ibf7c99 *
    
    Change-Id: I1d968b25f6d435b5fe68b8cec42b044a5c4f5661

--- src/ce/ve.ce.ResizableNode.js
+++ src/ce/ve.ce.ResizableNode.js
@@ -123,7 +123,7 @@ ve.ce.ResizableNode.prototype.hideSizeLabel = function () {
 	} );
 	// Actually hide the size label after it's done animating
 	setTimeout( function () {
-		node.$sizeLabel.hide();
+		node.$sizeLabel.addClass( 'oo-ui-element-hidden' );
 	}, 200 );
 };
 
@@ -150,7 +150,7 @@ ve.ce.ResizableNode.prototype.updateSizeLabel = function () {
 		height = dimensions.height;
 	}
 	this.$sizeLabel
-		.show()
+		.removeClass( 'oo-ui-element-hidden' )
 		.addClass( 've-ce-resizableNode-sizeLabel-resizing' )
 		.css( {
 			top: top,
--- src/ui/ve.ui.MobileContext.js
+++ src/ui/ve.ui.MobileContext.js
@@ -31,8 +31,7 @@ ve.ui.MobileContext = function VeUiMobileContext( surface, config ) {
 	this.$element
 		.addClass( 've-ui-mobileContext' )
 		.append( this.menu.$element )
-		// Mobile context uses a class to toggle visibility
-		.show();
+		.removeClass( 'oo-ui-element-hidden' );
 	this.menu.$element.addClass( 've-ui-mobileContext-menu' );
 	this.inspectors.$element.addClass( 've-ui-mobileContext-inspectors' );
 	this.surface.getGlobalOverlay().$element.append( this.inspectors.$element );
@@ -72,7 +71,9 @@ ve.ui.MobileContext.prototype.toggle = function ( show ) {
 	show = show === undefined ? !this.visible : !!show;
 	if ( show !== this.visible ) {
 		this.visible = show;
-		this.$element.toggleClass( 've-ui-mobileContext-visible', show );
+		this.$element
+			.toggleClass( 've-ui-mobileContext-visible', show );
+			.toggleClass( 'oo-ui-element-hidden', !show );
 		setTimeout( function () {
 			deferred.resolve();
 		}, 300 );
