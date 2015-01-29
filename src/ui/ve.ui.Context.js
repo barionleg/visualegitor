df04490f55e02b18d8b81a5891d585ab5b249c44
Moriel Schottlender <moriel@gmail.com>
2015-01-29 15:38:56 -0800

    Use css class rather than jQuery show/hide
    * Dependent on Ibf7c99 *
    
    Bug: T87420
    Change-Id: I1d968b25f6d435b5fe68b8cec42b044a5c4f5661

--- src/ui/ve.ui.Context.js
+++ src/ui/ve.ui.Context.js
@@ -38,7 +38,9 @@ ve.ui.Context = function VeUiContext( surface, config ) {
 	// Initialization
 	// Hide element using a class, not this.toggle, as child implementations
 	// of toggle may require the instance to be fully constructed before running.
-	this.$element.addClass( 've-ui-context' );
+	this.$element
+		.addClass( 've-ui-context' );
+	this.toggle( false );
 	this.menu.toggle( false );
 	this.inspectors.$element.addClass( 've-ui-context-inspectors' );
 };
@@ -324,7 +326,7 @@ ve.ui.Context.prototype.toggle = function ( show ) {
 	show = show === undefined ? !this.visible : !!show;
 	if ( show !== this.visible ) {
 		this.visible = show;
-		this.$element.toggle();
+		this.$element.toggleClass( 'oo-ui-element-hidden', !this.visible );
 	}
 	return $.Deferred().resolve().promise();
 };
