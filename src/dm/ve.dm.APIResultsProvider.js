c0056ce8b461cff84e5be908a7b7e988bf12cdc4
Moriel Schottlender <moriel@gmail.com>
2015-02-06 17:35:03 -0800

    Create APIResultsProvider and APIResultsQueue and add tests
    Generalize ResultsQueue and ResultsProvider downstream and
    create unit tests.
    
    Change-Id: I5346081317ef7195020b52deae322d70af80cae6

---
+++ src/dm/ve.dm.APIResultsProvider.js
@@ -0,0 +1,211 @@
+/*!
+ * VisualEditor DataModel ResourceProvider class.
+ *
+ * @copyright 2011-2015 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * Resource Provider object.
+ *
+ * @class
+ * @mixins OO.EventEmitter
+ *
+ * @constructor
+ * @param {string} apiurl The URL to the api
+ * @param {Object} [config] Configuration options
+ */
+ve.dm.APIResultsProvider = function VeDmResourceProvider( config ) {
+	config = config || {};
+
+	this.setAPIurl( config.apiurl );
+	this.fetchLimit = config.fetchLimit || 30;
+	this.lang = config.lang;
+	this.offset = config.offset || 0;
+	this.ajaxSettings = config.ajaxSettings || {};
+
+	this.staticParams = config.staticParams || {};
+	this.userParams = config.userParams || {};
+
+	this.toggleDepleted( false );
+
+	// Mixin constructors
+	OO.EventEmitter.call( this );
+};
+
+/* Setup */
+OO.initClass( ve.dm.APIResultsProvider );
+OO.mixinClass( ve.dm.APIResultsProvider, OO.EventEmitter );
+
+/* Methods */
+
+/**
+ * Get results from the source
+ *
+ * @param {number} howMany Number of results to ask for
+ * @return {jQuery.Promise} Promise that is resolved into an array
+ * of available results, or is rejected if no results are available.
+ */
+ve.dm.APIResultsProvider.prototype.getResults = function () {
+	var xhr,
+		deferred = $.Deferred(),
+		allParams = $.extend( {}, this.getStaticParams(), this.getUserParams() );
+
+	xhr = $.getJSON( this.getAPIurl(), allParams )
+		.done( function ( data ) {
+			if (
+				$.type( data ) !== 'array' ||
+				(
+					$.type( data ) === 'array' &&
+					data.length === 0
+				)
+			) {
+				deferred.resolve();
+			} else {
+				deferred.resolve( data );
+			}
+		} );
+	return deferred.promise( { abort: xhr.abort } );
+};
+
+/**
+ * Set api url
+ *
+ * @param {string} API url
+ */
+ve.dm.APIResultsProvider.prototype.setAPIurl = function ( url ) {
+	this.apiurl = url;
+};
+
+/**
+ * Set api url
+ *
+ * @returns {string} API url
+ */
+ve.dm.APIResultsProvider.prototype.getAPIurl = function () {
+	return this.apiurl;
+};
+
+/**
+ * Get the static, non-changing data parameters sent to the API
+ *
+ * @returns {Object} Data parameters
+ */
+ve.dm.APIResultsProvider.prototype.getStaticParams = function () {
+	return this.staticParams;
+};
+
+/**
+ * Get the user-inputted dybamic data parameters sent to the API
+ *
+ * @returns {Object} Data parameters
+ */
+ve.dm.APIResultsProvider.prototype.getUserParams = function () {
+	return this.userParams;
+};
+
+/**
+ * Set the data parameters sent to the API
+ *
+ * @param {Object} Data parameters
+ */
+ve.dm.APIResultsProvider.prototype.setUserParams = function ( params ) {
+	// Assymetrically compare (params is subset of this.userParams)
+	if ( !ve.compare( params, this.userParams, true ) ) {
+		this.userParams = $.extend( {}, this.userParams, params );
+		// Reset offset
+		this.setOffset( 0 );
+		// Reset depleted status
+		this.toggleDepleted( false );
+	}
+};
+
+/**
+ * Get fetch limit or 'page' size. This is the number
+ * of results per request.
+ *
+ * @returns {number} limit
+ */
+ve.dm.APIResultsProvider.prototype.getDefaultFetchLimit = function () {
+	return this.limit;
+};
+
+/**
+ * Set limit
+ *
+ * @param {number} limit
+ */
+ve.dm.APIResultsProvider.prototype.setDefaultFetchLimit = function ( limit ) {
+	this.limit = limit;
+};
+
+/**
+ * Get lang
+ *
+ * @returns {string} lang
+ */
+ve.dm.APIResultsProvider.prototype.getLang = function () {
+	return this.lang;
+};
+
+/**
+ * Set lang
+ *
+ * @param {string} lang
+ */
+ve.dm.APIResultsProvider.prototype.setLang = function ( lang ) {
+	this.lang = lang;
+};
+
+/**
+ * Get Offset
+ *
+ * @returns {number} Offset
+ */
+ve.dm.APIResultsProvider.prototype.getOffset = function () {
+	return this.offset;
+};
+
+/**
+ * Set Offset
+ *
+ * @param {number} Offset
+ */
+ve.dm.APIResultsProvider.prototype.setOffset = function ( offset ) {
+	this.offset = offset;
+};
+
+/**
+ * Check whether the provider is depleted
+ *
+ * @returns {boolean} depleted
+ */
+ve.dm.APIResultsProvider.prototype.isDepleted = function () {
+	return this.depleted;
+};
+
+/**
+ * Toggle depleted state
+ *
+ * @param {boolean} depleted
+ */
+ve.dm.APIResultsProvider.prototype.toggleDepleted = function ( isDepleted ) {
+	this.depleted = isDepleted !== undefined ? isDepleted : !this.depleted;
+};
+
+/**
+ * Get the default ajax settings
+ *
+ * @returns {Object} Ajax settings
+ */
+ve.dm.APIResultsProvider.prototype.getAjaxSettings = function () {
+	return this.ajaxSettings;
+};
+
+/**
+ * Get the default ajax settings
+ *
+ * @param {Object} Ajax settings
+ */
+ve.dm.APIResultsProvider.prototype.setAjaxSettings = function ( settings ) {
+	this.ajaxSettings = settings;
+};
