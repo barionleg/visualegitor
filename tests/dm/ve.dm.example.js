029aff70283703ac1dce90d6534463f6722fdb2d
Roan Kattouw <roan.kattouw@gmail.com>
2015-02-13 18:20:34 -0800

    Add round-trip test to getModelFromDom tests
    The getDomFromModel tests aren't true round-trip tests:
    they assert that the expected data maps back to the HTML,
    not that the actual model produced round-trips. This isn't
    a significant difference now, but it will matter in
    the near future when I replace htmlAttributes
    with originalDomElements.
    
    Change-Id: I9f06e7f4b2c93700e3b04eeb1ff7842fdfa5dd6a

--- tests/dm/ve.dm.example.js
+++ tests/dm/ve.dm.example.js
@@ -52,6 +52,32 @@ ve.dm.example.preprocessAnnotations = function ( data, store ) {
 };
 
 /**
+ * Convert real data back to shorthand notation. See #preprocessAnnotations
+ *
+ * @param {Array} data Linear model data. Will be modified.
+ * @param {ve.dm.IndexValueStore} store Index-value store to resolve annotations in
+ */
+ve.dm.example.postprocessAnnotations = function ( data, store ) {
+	var i, j, key;
+
+	for ( i = 0; i < data.length; i++ ) {
+		key = data[i].annotations ? 'annotations' : 1;
+		if ( Array.isArray( data[i][key] ) ) {
+			data[i] = $.extend( Array.isArray( data[i] ) ? [] : {}, data[i] );
+			data[i][key] = new ve.dm.AnnotationSet( store, data[i][key] ).get();
+			for ( j = 0; j < data[i][key].length; j++ ) {
+				data[i][key][j] = data[i][key][j].element;
+				if ( data[i][key][j].originalDomElements ) {
+					// Make a shallow clone and remove .originalDomElements from it
+					data[i][key][j] = $.extend( {}, data[i][key][j] );
+					delete data[i][key][j].originalDomElements;
+				}
+			}
+		}
+	}
+};
+
+/**
  * Create an annotation object from shorthand notation.
  * @method
  * @param {Object} annotation Plain object with type and attributes properties
