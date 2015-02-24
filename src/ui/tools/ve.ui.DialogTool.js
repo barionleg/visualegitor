8cab94edb3c89dd534f097d2ef6794a4d07841e0
Ed Sanders <esanders@wikimedia.org>
2015-02-24 17:27:32 +0000

    Move special character inserter to toolbar dialog manager
    Change-Id: I991cde8214895e0a689b3d21c5cdb70a3d164dd6

--- src/ui/tools/ve.ui.DialogTool.js
+++ src/ui/tools/ve.ui.DialogTool.js
@@ -14,9 +14,9 @@
  * @param {OO.ui.ToolGroup} toolGroup
  * @param {Object} [config] Configuration options
  */
-ve.ui.DialogTool = function VeUiDialogTool( toolGroup, config ) {
+ve.ui.DialogTool = function VeUiDialogTool() {
 	// Parent constructor
-	ve.ui.Tool.call( this, toolGroup, config );
+	ve.ui.DialogTool.super.apply( this, arguments );
 };
 
 /* Inheritance */
@@ -56,14 +56,16 @@ ve.ui.DialogTool.prototype.onUpdateState = function () {
 };
 
 /**
+ * Command help tool.
+ *
  * @class
  * @extends ve.ui.DialogTool
  * @constructor
  * @param {OO.ui.ToolGroup} toolGroup
  * @param {Object} [config] Configuration options
  */
-ve.ui.CommandHelpDialogTool = function VeUiCommandHelpDialogTool( toolGroup, config ) {
-	ve.ui.DialogTool.call( this, toolGroup, config );
+ve.ui.CommandHelpDialogTool = function VeUiCommandHelpDialogTool() {
+	ve.ui.CommandHelpDialogTool.super.apply( this, arguments );
 };
 OO.inheritClass( ve.ui.CommandHelpDialogTool, ve.ui.DialogTool );
 ve.ui.CommandHelpDialogTool.static.name = 'commandHelp';
@@ -75,3 +77,26 @@ ve.ui.CommandHelpDialogTool.static.autoAddToCatchall = false;
 ve.ui.CommandHelpDialogTool.static.autoAddToGroup = false;
 ve.ui.CommandHelpDialogTool.static.commandName = 'commandHelp';
 ve.ui.toolFactory.register( ve.ui.CommandHelpDialogTool );
+
+/**
+ * Special character tool.
+ *
+ * @class
+ * @extends ve.ui.DialogTool
+ * @constructor
+ * @param {OO.ui.ToolGroup} toolGroup
+ * @param {Object} [config] Configuration options
+ */
+ve.ui.SpecialCharacterDialogTool = function VeUiSpecialCharacterDialogTool() {
+	ve.ui.SpecialCharacterDialogTool.super.apply( this, arguments );
+};
+OO.inheritClass( ve.ui.SpecialCharacterDialogTool, ve.ui.DialogTool );
+ve.ui.SpecialCharacterDialogTool.static.name = 'specialCharacter';
+ve.ui.SpecialCharacterDialogTool.static.group = 'dialog';
+ve.ui.SpecialCharacterDialogTool.static.icon = 'special-character';
+ve.ui.SpecialCharacterDialogTool.static.title =
+	OO.ui.deferMsg( 'visualeditor-specialcharacter-button-tooltip' );
+ve.ui.SpecialCharacterDialogTool.static.autoAddToCatchall = false;
+ve.ui.SpecialCharacterDialogTool.static.autoAddToGroup = false;
+ve.ui.SpecialCharacterDialogTool.static.commandName = 'specialCharacter';
+ve.ui.toolFactory.register( ve.ui.SpecialCharacterDialogTool );
