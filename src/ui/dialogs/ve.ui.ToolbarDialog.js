de2c7ade33c58dd45162e7e140a35c3a39b1d7ea
Ed Sanders <esanders@wikimedia.org>
2015-02-24 14:48:30 +0000

    Move special character inserter to toolbar dialog manager
    TODO: Work out where to put a close/done button.
    
    Change-Id: I991cde8214895e0a689b3d21c5cdb70a3d164dd6

--- src/ui/dialogs/ve.ui.ToolbarDialog.js
+++ src/ui/dialogs/ve.ui.ToolbarDialog.js
@@ -33,6 +33,8 @@ OO.inheritClass( ve.ui.ToolbarDialog, OO.ui.Dialog );
 
 ve.ui.ToolbarDialog.static.size = 'full';
 
+ve.ui.ToolbarDialog.static.activeSurface = true;
+
 ve.ui.ToolbarDialog.static.padded = true;
 
 /* Methods */
