930f83a98175f7f2243291e4e58d16a490871471
Ed Sanders <esanders@wikimedia.org>
2015-03-03 23:38:11 +0000

    Only store linear selections as last context position
    Also clear when the context is closed.
    
    Bug: T91307
    Change-Id: I3c62d68b357bac094ed4cf50eb35fef45606c5af

--- src/ui/ve.ui.DesktopContext.js
+++ src/ui/ve.ui.DesktopContext.js
@@ -38,7 +38,8 @@ ve.ui.DesktopContext = function VeUiDesktopContext( surface, config ) {
 		select: 'onModelSelect'
 	} );
 	this.inspectors.connect( this, {
-		resize: 'setPopupSize'
+		resize: 'setPopupSize',
+		closing: 'onInspectorClosing'
 	} );
 	this.$window.on( 'resize', this.onWindowResizeHandler );
 
@@ -143,6 +144,13 @@ ve.ui.DesktopContext.prototype.onInspectorOpening = function () {
 };
 
 /**
+ * Handle inspector closing events.
+ */
+ve.ui.DesktopContext.prototype.onInspectorClosing = function () {
+	this.lastClosedSelection = null;
+};
+
+/**
  * @inheritdoc
  */
 ve.ui.DesktopContext.prototype.toggle = function ( show ) {
@@ -190,7 +198,10 @@ ve.ui.DesktopContext.prototype.updateDimensions = function () {
 		surface = this.surface.getView(),
 		focusedNode = surface.getFocusedNode();
 
-	if ( !this.inspector || ( !this.inspector.isOpening() && !this.inspector.isOpened() ) ) {
+	if (
+		( !this.inspector || ( !this.inspector.isOpening() && !this.inspector.isOpened() ) ) &&
+		surface.getModel().getSelection() instanceof ve.dm.LinearSelection
+	) {
 		this.lastClosedSelection = surface.getModel().getSelection().clone();
 	}
 	boundingRect = surface.getSelectionBoundingRect( this.lastClosedSelection );
