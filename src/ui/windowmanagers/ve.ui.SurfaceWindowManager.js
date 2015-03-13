3a385f4a06e42cb00ea6b3a9acd7aebebc36d9ec
Moriel Schottlender <moriel@gmail.com>
2015-03-13 14:08:35 -0700

    Add directionality classes to window manager
    Many of the icon settings require the directionality class in
    order to feature icons that can be flipped between ltr/rtl. Those
    icons don't show inside dialogs and inspectors unless the wrapping
    window manager holds those classes.
    
    In this fix we set the directionality of SurfaceWindowManager
    based on the surface, and the directionality of the WindowManager
    based on the body element direction.
    
    Change-Id: Ibf29417a2eb64d966417cfa96a0d4417e3d80efd

--- src/ui/windowmanagers/ve.ui.SurfaceWindowManager.js
+++ src/ui/windowmanagers/ve.ui.SurfaceWindowManager.js
@@ -32,6 +32,16 @@ OO.inheritClass( ve.ui.SurfaceWindowManager, ve.ui.WindowManager );
 /* Methods */
 
 /**
+ * Override the window manager's directionality method to get the
+ * directionality from the surface. The surface sometimes does not
+ * have a directionality set; fallback to direction from the document.
+ * @return {string} UI directionality
+ */
+ve.ui.SurfaceWindowManager.prototype.getDir = function () {
+	return this.surface.getDir() || $( 'body' ).css( 'direction' );
+};
+
+/**
  * Get surface.
  *
  * @return {ve.ui.Surface} Surface this belongs to
