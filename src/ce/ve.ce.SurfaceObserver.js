040404f037b8acb6a6d2982c3b5a23baf6d27bea
Ed Sanders <esanders@wikimedia.org>
2015-03-21 12:37:52 +0000

    Fix RangeState detection of selection outisde of document
    Previous logic was mistakenly treating 'selection' as if it
    was a real native selection, which it isn't. Fix logic to use
    the correct variable, and detect if you are inside the document
    node the same way we do in ve.ce.Surface#onFocusChange.
    
    Refactor RangeState slightly to pass in the whole ce.Surface
    instead of surface.$element and the surface's documentNode.
    
    Change-Id: I37903f4f79fa77d1cf1def6b30f90c0592675c74

--- src/ce/ve.ce.SurfaceObserver.js
+++ src/ce/ve.ce.SurfaceObserver.js
@@ -206,8 +206,7 @@ ve.ce.SurfaceObserver.prototype.pollOnceInternal = function ( emitChanges, selec
 	oldState = this.rangeState;
 	newState = new ve.ce.RangeState(
 		oldState,
-		this.surface.$element,
-		this.documentView.getDocumentNode(),
+		this.surface,
 		selectionOnly
 	);
 
