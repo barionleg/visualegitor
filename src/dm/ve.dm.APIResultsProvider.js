63d00e471e8cd0669ce623848ceeb84478f2ee9a
Moriel Schottlender <moriel@gmail.com>
2015-05-18 13:36:29 -0400

    Small typo fixes for ve.dm.APIResultsProvider
    Make sure fetch limit is actually stored by using its setter,
    and fix a small typo in documentation.
    
    Change-Id: I1c2f7d5f2f38f4b08e3ec1fe6af01e36f6b26dde

--- src/dm/ve.dm.APIResultsProvider.js
+++ src/dm/ve.dm.APIResultsProvider.js
@@ -27,7 +27,7 @@ ve.dm.APIResultsProvider = function VeDmResourceProvider( apiurl, config ) {
 	config = config || {};
 
 	this.setAPIurl( apiurl );
-	this.fetchLimit = config.fetchLimit || 30;
+	this.setDefaultFetchLimit( config.fetchLimit || 30 );
 	this.lang = config.lang;
 	this.offset = config.offset || 0;
 	this.ajaxSettings = config.ajaxSettings || {};
@@ -103,7 +103,7 @@ ve.dm.APIResultsProvider.prototype.getStaticParams = function () {
 };
 
 /**
- * Get the user-inputted dybamic data parameters sent to the API
+ * Get the user-inputted dynamic data parameters sent to the API
  *
  * @returns {Object} Data parameters
  */
