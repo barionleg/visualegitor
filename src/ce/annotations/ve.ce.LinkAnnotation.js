3cea37e434c995151c7550d7e932778811d0c129
David Chan <david@sheetmusic.org.uk>
2015-06-02 17:47:33 +0800

    WIP: Explicitly enter/exit link annotations
    Link anchor tags are "nailed" with surrounding img nodes
    
    Idea refinement progress:
    0. Fail to prove it can't work
    1. Make it work
    [we are approximately here]
    2. Make it clean
    3. Make it efficient
    
    TODO:
    - Cleanup
    - Decide what to do about ve.ce.getDomHash
    - Implement ve.hasHardCursorBoundaries and ve.rejectsCursor properly
    - Better names for functions and for the 'nail' concept
    - Debug image, and non-debug invisibility, for annotation nails
    - Better visualisation to show the cursor is inside the link
    - Make cursor land inside text node, to avoid Firefox disappearing cursor
      (https://bugzilla.mozilla.org/show_bug.cgi?id=1175495)
      (would also ameliorate https://bugzilla.mozilla.org/show_bug.cgi?id=1175418)
    
    Change-Id: Ie643984c24168ae31d37e0eae8e9003b63ac4f18

--- src/ce/annotations/ve.ce.LinkAnnotation.js
+++ src/ce/annotations/ve.ce.LinkAnnotation.js
@@ -19,12 +19,19 @@ ve.ce.LinkAnnotation = function VeCeLinkAnnotation() {
 	ve.ce.LinkAnnotation.super.apply( this, arguments );
 
 	// Initialization
-	this.$element
-		.addClass( 've-ce-linkAnnotation' )
+	this.contentFragment = document.createDocumentFragment();
+
+	this.$anchor = $( '<a>' )
 		.prop( {
 			href: ve.resolveUrl( this.model.getHref(), this.getModelHtmlDocument() ),
 			title: this.constructor.static.getDescription( this.model )
 		} );
+
+	this.$element
+		.addClass( 've-ce-linkAnnotation' )
+		.append( this.constructor.static.makePreNail() )
+		.append( this.$anchor )
+		.append( this.constructor.static.makePostNail() );
 };
 
 /* Inheritance */
@@ -35,9 +42,7 @@ OO.inheritClass( ve.ce.LinkAnnotation, ve.ce.Annotation );
 
 ve.ce.LinkAnnotation.static.name = 'link';
 
-ve.ce.LinkAnnotation.static.tagName = 'a';
-
-ve.ce.LinkAnnotation.static.forceContinuation = true;
+ve.ce.LinkAnnotation.static.tagName = 'span';
 
 /* Static Methods */
 
@@ -46,6 +51,38 @@ ve.ce.LinkAnnotation.static.forceContinuation = true;
  */
 ve.ce.LinkAnnotation.static.getDescription = function ( model ) {
 	return model.getHref();
+};
+
+ve.ce.LinkAnnotation.static.makePreNail = function () {
+	return $( '<img>' )
+		.prop( 'src', ve.ce.chimeraImgDataUri )
+		.addClass( 've-ce-nail' )
+		.addClass( 've-ce-pre-nail' )
+		.get( 0 );
+};
+
+ve.ce.LinkAnnotation.static.makePostNail = function () {
+	return $( '<img>' )
+		.prop( 'src', ve.ce.chimeraImgDataUri )
+		.addClass( 've-ce-nail' )
+		.addClass( 've-ce-post-nail' )
+		.get( 0 );
+};
+
+/* Methods */
+
+ve.ce.LinkAnnotation.prototype.getContentContainer = function () {
+	return this.contentFragment;
+};
+
+/**
+ * Attach contents to the annotation as descendent nodes, if not already attached
+ */
+ve.ce.LinkAnnotation.prototype.attachContents = function () {
+	this.$anchor
+		.append( this.constructor.static.makePostNail() )
+		.append( this.contentFragment )
+		.append( this.constructor.static.makePreNail() );
 };
 
 /* Registration */
