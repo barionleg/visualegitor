75b214911464a55f3dd20f3c73e1349b400fb17d
C. Scott Ananian <cscott@cscott.net>
2015-08-07 13:49:34 -0400

    Support RegExp sequences; trigger sequence matcher after newline.
    Allow a ve.ui.Sequence to be specified using a RegExp, but hard-limit the
    size of the string checked by the RegExp to avoid O(N^2) blowup when
    inserting N characters of text.
    
    Tweak the signatures of the sequence match methods to return the range of
    the matching portion of the data, to avoid having to repeat the text
    extraction and regexp match when executing the associated command.
    Allow passing this range as the current selection when executing the
    sequence's associated command.
    
    Trigger `checkSequences` in `handleLinearEnter` so that sequences
    are checked when enter is pressed (pressing enter doesn't trigger
    onSurfaceObserverContentChange).
    
    Change-Id: I4d03d2f7b437ea4a289a4c5cee2a081998228319

--- src/ce/ve.ce.Surface.js
+++ src/ce/ve.ce.Surface.js
@@ -2826,7 +2826,7 @@ ve.ce.Surface.prototype.checkSequences = function () {
 
 	// sequences.length will likely be 0 or 1 so don't cache
 	for ( i = 0; i < sequences.length; i++ ) {
-		executed = sequences[i].execute( this.surface ) || executed;
+		executed = sequences[i].sequence.execute( this.surface, sequences[i].range ) || executed;
 	}
 	if ( executed ) {
 		this.showModelSelection( model.getSelection() );
@@ -3418,7 +3418,8 @@ ve.ce.Surface.prototype.handleLinearEnter = function ( e ) {
 		stack = [],
 		outermostNode = null,
 		nodeModel = null,
-		nodeModelRange = null;
+		nodeModelRange = null,
+		surface = this;
 
 	// Handle removal first
 	if ( !range.isCollapsed() ) {
@@ -3579,6 +3580,9 @@ ve.ce.Surface.prototype.handleLinearEnter = function ( e ) {
 	}
 	// Reset and resume polling
 	this.surfaceObserver.clear();
+	setTimeout( function () {
+		surface.checkSequences();
+	} );
 };
 
 /**
