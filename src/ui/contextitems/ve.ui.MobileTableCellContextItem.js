1e6dab0fe8816f83ab1e8ab6dc9ced2c7d65b62b
Ed Sanders <esanders@wikimedia.org>
2015-08-09 13:08:00 +0100

    Provide a context item for table cells in mobile
    Mobile doesn't provide double click or an enter key, which
    desktop uses for editing table cells, so provide a context
    item intead so the user can tap 'edit'.
    
    Bug: T108514
    Change-Id: Iaec408d195de4975a275130ef6da3f2d7b0ec126

---
+++ src/ui/contextitems/ve.ui.MobileTableCellContextItem.js
@@ -0,0 +1,54 @@
+/*!
+ * VisualEditor MobileTableCellContextItem class.
+ *
+ * @copyright 2011-2015 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * Context item for a table cell in mobile.
+ *
+ * @class
+ * @extends ve.ui.ContextItem
+ *
+ * @param {ve.ui.Context} context Context item is in
+ * @param {ve.dm.Model} model Model item is related to
+ * @param {Object} config Configuration options
+ */
+ve.ui.MobileTableCellContextItem = function VeMobileTableCellContextItem() {
+	// Parent constructor
+	ve.ui.MobileTableCellContextItem.super.apply( this, arguments );
+
+	// Initialization
+	this.$element.addClass( 've-ui-mobileTableCellContextItem' );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ui.MobileTableCellContextItem, ve.ui.ContextItem );
+
+/* Static Properties */
+
+ve.ui.MobileTableCellContextItem.static.name = 'tableCell';
+
+ve.ui.MobileTableCellContextItem.static.icon = 'table';
+
+ve.ui.MobileTableCellContextItem.static.label = OO.ui.deferMsg( 'visualeditor-tablecell-contextitem' );
+
+ve.ui.MobileTableCellContextItem.static.modelClasses = [ ve.dm.TableCellNode ];
+
+ve.ui.MobileTableCellContextItem.static.embeddable = false;
+
+ve.ui.MobileTableCellContextItem.static.commandName = 'enterTableCell';
+
+/* Methods */
+
+/**
+ * @inheritdoc
+ */
+ve.ui.MobileTableCellContextItem.prototype.getDescription = function () {
+	//return this.model.getAttribute( 'text' ).trim();
+};
+
+/* Registration */
+
+ve.ui.contextItemFactory.register( ve.ui.MobileTableCellContextItem );
