1a193537a65db9b3906c54900f843ebbeb0782a6
Ed Sanders <esanders@wikimedia.org>
2015-08-10 19:59:54 +0100

    Delete button for node context items
    Bug: T62110
    Change-Id: I41109537d2732f85622991871ee1bf4063808cbd

--- src/ui/ve.ui.ContextItem.js
+++ src/ui/ve.ui.ContextItem.js
@@ -31,6 +31,7 @@ ve.ui.ContextItem = function ( context, model, config ) {
 	// Properties
 	this.context = context;
 	this.model = model;
+	this.isNode = model && model instanceof ve.dm.Node;
 	this.$head = $( '<div>' );
 	this.$title = $( '<div>' );
 	this.$actions = $( '<div>' );
@@ -45,10 +46,25 @@ ve.ui.ContextItem = function ( context, model, config ) {
 			classes: [ 've-ui-contextItem-editButton' ]
 		}
 	) );
+	this.deleteButton = new OO.ui.ButtonWidget( ve.extendObject(
+		this.context.getButtonConfig(),
+		{
+			icon: 'remove',
+			flags: [ 'destructive' ]
+		}
+	) );
+	this.actionButtons = new OO.ui.ButtonGroupWidget();
+	if ( this.isNode ) {
+		this.actionButtons.addItems( [ this.deleteButton ] );
+	}
+	if ( this.isEditable() ) {
+		this.actionButtons.addItems( [ this.editButton ] );
+	}
 	this.fragment = null;
 
 	// Events
 	this.editButton.connect( this, { click: 'onEditButtonClick' } );
+	this.deleteButton.connect( this, { click: 'onDeleteButtonClick' } );
 	this.$element.on( 'mousedown', false );
 
 	// Initialization
@@ -63,7 +79,7 @@ ve.ui.ContextItem = function ( context, model, config ) {
 		.append( this.$icon, this.$label );
 	this.$actions
 		.addClass( 've-ui-contextItem-actions' )
-		.append( this.editButton.$element );
+		.append( this.actionButtons.$element );
 	this.$head
 		.addClass( 've-ui-contextItem-head' )
 		.append( this.$title, this.$info, this.$actions );
@@ -142,6 +158,13 @@ ve.ui.ContextItem.prototype.onEditButtonClick = function () {
 };
 
 /**
+ * Handle delete button click events.
+ */
+ve.ui.ContextItem.prototype.onDeleteButtonClick = function () {
+	this.getFragment().removeContent();
+};
+
+/**
  * Check if this item is compatible with a given model.
  *
  * @static
@@ -179,7 +202,7 @@ ve.ui.ContextItem.prototype.getCommand = function () {
 ve.ui.ContextItem.prototype.getFragment = function () {
 	if ( !this.fragment ) {
 		var surfaceModel = this.context.getSurface().getModel();
-		this.fragment = this.model && this.model instanceof ve.dm.Node ?
+		this.fragment = this.isNode ?
 			surfaceModel.getLinearFragment( this.model.getOuterRange() ) :
 			surfaceModel.getFragment();
 	}
@@ -222,8 +245,6 @@ ve.ui.ContextItem.prototype.renderDescription = function () {
  * @chainable
  */
 ve.ui.ContextItem.prototype.setup = function () {
-	this.editButton.toggle( this.isEditable() );
-
 	if ( this.context.shouldUseBasicRendering() ) {
 		this.renderDescription();
 	} else {
