a911a20869499ddd09ac40209fbfbbde777a8d98
David Lynch <dlynch@wikimedia.org>
2015-10-20 02:47:45 -0500

    ce.Surface afterKeyDown shouldn't assume .contenteditable is boolean
    Turns out we were checking .prop('contenteditable') and assuming it'd
    be a boolean, when it was really a string ('true'/'false'/'inherit').
    So, switch to using .isContentEditable, which handles all that for us.
    
    Primary impact: this now better copes with adjacent grouped nodes.
    If a document contains adjacent nodes in a group, cursor navigation
    would only sometimes treat them as a group. Moving to after the group
    and pressing left, in particular, would easily get the cursor in the
    middle of the group. The cursor is _technically_ not in a non-editable
    location at this time, but in practice it should be treated as being
    so.
    
    Bug: T114545
    Change-Id: I4452aa05753fdacd29b861c058ec62f9ca918a8f

--- src/ce/ve.ce.Surface.js
+++ src/ce/ve.ce.Surface.js
@@ -1350,6 +1350,8 @@ ve.ce.Surface.prototype.afterDocumentKeyDown = function ( e ) {
 	 * If ce=false then we have landed inside the focusable node.
 	 * If we land in a non-text position, assume we should have hit the node
 	 * immediately after the position we hit (in the direction of motion)
+	 * If we land inside a sequence of grouped nodes, assume we should treat them as a
+	 * unit instead of letting the cursor slip inside them.
 
 	 * @private
 	 * @param {Node} DOM node of cursor position
@@ -1358,7 +1360,7 @@ ve.ce.Surface.prototype.afterDocumentKeyDown = function ( e ) {
 	 * @return {ve.ce.Node|null} node, or null if not in a focusable node
 	 */
 	function getSurroundingFocusableNode( node, offset, direction ) {
-		var focusNode, $focusNode;
+		var focusNode;
 		if ( node.nodeType === Node.TEXT_NODE ) {
 			focusNode = node;
 		} else if ( direction > 0 && offset < node.childNodes.length ) {
@@ -1368,13 +1370,10 @@ ve.ce.Surface.prototype.afterDocumentKeyDown = function ( e ) {
 		} else {
 			focusNode = node;
 		}
-		$focusNode = $( focusNode );
-		// If the first ancestor with contenteditable set is ce=true, then we are allowed
-		// to be inside this focusalbe node (e.g. editing a table cell or caption)
-		if ( $focusNode.closest( '[contenteditable]' ).prop( 'contenteditable' ) ) {
+		if ( ( focusNode.nodeType === Node.TEXT_NODE ? focusNode.parentNode : focusNode ).isContentEditable ) {
 			return null;
 		}
-		return $focusNode.closest( '.ve-ce-focusableNode, .ve-ce-tableNode' ).data( 'view' ) || null;
+		return $( focusNode ).closest( '.ve-ce-focusableNode, .ve-ce-tableNode' ).data( 'view' ) || null;
 	}
 
 	/**
