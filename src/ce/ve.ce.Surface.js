17bfdeacde3c0b465bef925aef3b8eadbc31aa18
David Chan <david@troi.org>
2015-10-22 18:11:25 +0800

    Reapply the cursor after delete to kill browser preannotations
    Bug: T116269
    Change-Id: I05b252ceb6a791a6ab07c986a4f13641cdb15820

--- src/ce/ve.ce.Surface.js
+++ src/ce/ve.ce.Surface.js
@@ -1331,7 +1331,7 @@ ve.ce.Surface.prototype.onDocumentKeyPress = function ( e ) {
  */
 ve.ce.Surface.prototype.afterDocumentKeyDown = function ( e ) {
 	var direction, focusableNode, startOffset, endOffset, offsetDiff, dmFocus, dmSelection,
-		ceNode, range, fixupCursorForUnicorn, matrix, $focusNode,
+		inNonSlug, ceSelection, ceNode, range, fixupCursorForUnicorn, matrix, $focusNode,
 		surface = this,
 		isArrow = (
 			e.keyCode === OO.ui.Keys.UP ||
@@ -1405,27 +1405,47 @@ ve.ce.Surface.prototype.afterDocumentKeyDown = function ( e ) {
 
 	if (
 		( e.keyCode === OO.ui.Keys.BACKSPACE || e.keyCode === OO.ui.Keys.DELETE ) &&
-		this.nativeSelection.focusNode &&
-		this.nativeSelection.focusNode.nodeType === Node.ELEMENT_NODE &&
-		!this.nativeSelection.focusNode.classList.contains( 've-ce-branchNode-inlineSlug' )
+		this.nativeSelection.focusNode
 	) {
-		// In a non-slug element. Sync the DM, then see if we need a slug.
-		this.incRenderLock();
-		try {
-			this.surfaceObserver.pollOnce();
-		} finally {
-			this.decRenderLock();
-		}
+		inNonSlug = this.nativeSelection.focusNode.nodeType === Node.ELEMENT_NODE &&
+			!this.nativeSelection.focusNode.classList.contains( 've-ce-branchNode-inlineSlug' );
+		if ( inNonSlug ) {
+			// In a non-slug element. Sync the DM, then see if we need a slug.
+			this.incRenderLock();
+			try {
+				this.surfaceObserver.pollOnce();
+			} finally {
+				this.decRenderLock();
+			}
 
-		dmSelection = surface.model.getSelection();
-		if ( dmSelection instanceof ve.dm.LinearSelection ) {
-			dmFocus = dmSelection.getRange().end;
-			ceNode = this.documentView.getBranchNodeFromOffset( dmFocus );
-			if ( ceNode && ceNode.getModel().hasSlugAtOffset( dmFocus ) ) {
-				ceNode.setupBlockSlugs();
+			dmSelection = surface.model.getSelection();
+			if ( dmSelection instanceof ve.dm.LinearSelection ) {
+				dmFocus = dmSelection.getRange().end;
+				ceNode = this.documentView.getBranchNodeFromOffset( dmFocus );
+				if ( ceNode && ceNode.getModel().hasSlugAtOffset( dmFocus ) ) {
+					ceNode.setupBlockSlugs();
+				}
 			}
 		}
-		return;
+
+		// Remove then re-set the selection, to clear any browser-native preannotations.
+		// This should be IME-safe because delete/backspace key events should only happen
+		// when there is no IME candidate window open.
+		//
+		// Note that if an IME removes text otherwise than by delete/backspace, then
+		// browser-native preannotations might possibly survive. This could happen if you
+		// selected all the text in an annotation node, then started typing over it (so it
+		// got replaced with candidate text), *then* the IME deleted the candidate text
+		// (e.g. because you backspaced the candidate in the IME (but with the browser
+		// never seeing a backspace event, only a composition/input event)).
+		// This is nasty, but it's not a reason not to fix the delete/backspace case.
+		ceSelection = new ve.SelectionState( this.nativeSelection );
+		this.nativeSelection.removeAllRanges();
+		this.showSelectionState( ceSelection );
+
+		if ( inNonSlug ) {
+			return;
+		}
 	}
 
 	if ( e !== this.keyDownState.event ) {
