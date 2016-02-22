d8222ca98995341f181adbbc4253516c49272147
David Lynch <dlynch@wikimedia.org>
2016-02-22 12:22:14 -0600

    LinkContextItem: add label information to the context
    Adds a preview of the current label, and a button which quickly selects the
    entire contents of the link annotation. This makes it far easier to select the
    entire label for editing, without accidentally selecting outside the
    annotation and thus removing it.
    
    Bug: T124305
    Change-Id: Iad48fb559f3dae16cc0013791b79ec71308a1f9f

--- src/ui/contextitems/ve.ui.LinkContextItem.js
+++ src/ui/contextitems/ve.ui.LinkContextItem.js
@@ -20,6 +20,21 @@ ve.ui.LinkContextItem = function VeUiLinkContextItem( context, model, config ) {
 
 	// Initialization
 	this.$element.addClass( 've-ui-linkContextItem' );
+
+	this.labelPreview = new OO.ui.LabelWidget();
+
+	this.labelLayout = new OO.ui.HorizontalLayout( {
+		items: [
+			new OO.ui.IconWidget( { icon: 'quotes' } ),
+			new OO.ui.LabelWidget( { label: OO.ui.deferMsg( 'visualeditor-linkcontext-label-label' ) } ),
+			this.labelPreview,
+			new OO.ui.ButtonWidget( {
+				label: OO.ui.deferMsg( 'visualeditor-linkcontext-label-change' ),
+				framed: false,
+				flags: [ 'progressive' ]
+			} ).connect( this, { click: 'onLabelButtonClick' } )
+		]
+	} );
 };
 
 /* Inheritance */
@@ -63,8 +78,21 @@ ve.ui.LinkContextItem.prototype.renderBody = function () {
 				href: ve.resolveUrl( this.model.getHref(), htmlDoc ),
 				target: '_blank',
 				rel: 'noopener'
-			} )
+			} ),
+		this.labelLayout.$element
 	);
+	this.labelPreview.setLabel( this.context.getSurface().getView().activeLink.innerText.trim() || ve.msg( 'visualeditor-linkcontext-label-fallback' ) );
+};
+
+/**
+ * Handle label-edit button click events.
+ *
+ * @localdoc Selects the contents of the link annotation
+ *
+ * @protected
+ */
+ve.ui.LinkContextItem.prototype.onLabelButtonClick = function () {
+	this.context.getSurface().getView().selectActiveLinkContents();
 };
 
 /* Registration */
