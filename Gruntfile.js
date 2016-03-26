3e9317188bab4e4ef09caccbb7bdea84b4674812
Paladox <thomasmulhall410@yahoo.com>
2016-03-26 17:01:15 +0000

    Publish VisualEditors demos
    Includes a new index.html in demos/ so that it makes it really easy to
    choose the demo you want to try.
    
    THis also adds a script to allow us to publish to doc.wikimedia.org
    
    Change-Id: I6a7a93eda01f306a49d76e223d9e5976566890cb

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,7 +46,8 @@ module.exports = function ( grunt ) {
 	grunt.initConfig( {
 		pkg: grunt.file.readJSON( 'package.json' ),
 		clean: {
-			dist: [ 'dist/*', 'coverage/*' ]
+			dist: [ 'dist/*', 'coverage/*', 'demos/**' ],
+			demos: 'demos/{dist,src,lib,i18n,ve}'
 		},
 		concat: {
 			js: {
@@ -114,6 +115,12 @@ module.exports = function ( grunt ) {
 			lib: {
 				src: [ 'lib/**', '!lib/jquery.i18n/**', '!lib/jquery.uls/**' ],
 				dest: 'dist/',
+				expand: true
+			},
+			demos: {
+				// Make sure you update this if dependencies are added
+				src: '{dist/**/*,src/**/*,i18n/*.json,lib/**/*}',
+				dest: 'demos/',
 				expand: true
 			}
 		},
@@ -245,6 +252,9 @@ module.exports = function ( grunt ) {
 				'!lib/**',
 				'!i18n/**',
 				'!{coverage,dist,docs,node_modules}/**',
+				'!demos/lib/**',
+				'!demos/src/**',
+				'!demos/dist/**',
 				'!.git/**'
 			]
 		},
@@ -354,13 +364,14 @@ module.exports = function ( grunt ) {
 		} );
 	} );
 
-	grunt.registerTask( 'build', [ 'clean', 'concat', 'cssjanus', 'cssUrlEmbed', 'copy', 'buildloader' ] );
+	grunt.registerTask( 'build', [ 'clean', 'concat', 'cssjanus', 'cssUrlEmbed', 'copy:i18n', 'copy:lib', 'buildloader' ] );
 	grunt.registerTask( 'lint', [ 'tyops', 'jshint', 'jscs:main', 'csslint', 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'unit', [ 'karma:main' ] );
 	grunt.registerTask( 'fix', [ 'jscs:fix' ] );
 	grunt.registerTask( '_test', [ 'lint', 'git-build', 'build', 'unit' ] );
 	grunt.registerTask( 'ci', [ '_test', 'git-status' ] );
 	grunt.registerTask( 'watch', [ 'karma:bg:start', 'runwatch' ] );
+	grunt.registerTask( 'publish-build', [ 'clean:demos', 'concat', 'cssjanus', 'cssUrlEmbed', 'copy:demos', 'buildloader' ] );
 
 	if ( process.env.JENKINS_HOME ) {
 		grunt.registerTask( 'test', 'ci' );
