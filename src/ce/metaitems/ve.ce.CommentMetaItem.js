a1561de7e160a653781103151209fee27ff80f6c
David Chan <david@troi.org>
2016-06-28 16:08:56 +0100

    WIP: Store metadata as block nodes in the main data list
    TODO: improve the logic for moving metadata on branch merge
    
    Change-Id: I6f92011c27f334b132af290fbf8c85a6808f6472

---
+++ src/ce/metaitems/ve.ce.CommentMetaItem.js
@@ -0,0 +1,50 @@
+/*!
+ * VisualEditor ContentEditable CommentMetaItem class.
+ *
+ * @copyright 2011-2017 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * ContentEditable comment meta item node.
+ *
+ * @class
+ * @extends ve.ce.MetaItem
+ * @constructor
+ * @param {ve.dm.CommentMetaItem} model Model to observe
+ * @param {Object} [config] Configuration options
+ */
+ve.ce.CommentMetaItem = function VeCeCommentMetaItem() {
+	// Parent constructor
+	ve.ce.CommentMetaItem.super.apply( this, arguments );
+
+	// DOM changes
+	this.$element
+                .addClass( 've-ce-commentMetaItem' )
+                .prop( 'contentEditable', 'false' )
+		.append( ve.copyDomElements( this.model.getOriginalDomElements(
+			this.model.getDocument().getStore() ),
+			document
+		) );
+
+	// Mixin constructors
+	ve.ce.FocusableNode.call( this, this.$element, {
+		classes: [ 've-ce-commentMetaItem-highlights' ]
+	} );
+	ve.ce.TableCellableNode.call( this );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ce.CommentMetaItem, ve.ce.MetaItem );
+OO.mixinClass( ve.ce.CommentMetaItem, ve.ce.FocusableNode );
+OO.mixinClass( ve.ce.CommentMetaItem, ve.ce.TableCellableNode );
+
+/* Static Properties */
+
+ve.ce.CommentMetaItem.static.name = 'commentMeta';
+
+ve.ce.CommentMetaItem.static.iconWhenInvisible = 'puzzle';
+
+/* Registration */
+
+ve.ce.nodeFactory.register( ve.ce.CommentMetaItem );
