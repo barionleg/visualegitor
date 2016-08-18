414a11ef1deb1bd3b106259f6ed77d9f79fac61b
David Chan <david@troi.org>
2016-08-18 12:14:51 -0700

    WIP: Rebaser
    Change-Id: I068447d8155b0732c08d642357dab07cfca5aa2a

--- src/dm/ve.dm.Change.js
+++ src/dm/ve.dm.Change.js
@@ -1,3 +1,13 @@
+( function ( createClass ) {
+	if ( typeof window !== 'undefined' ) {
+		// Browser.
+		createClass( window.ve );
+	} else {
+		// Node. ve={dm:{}}; require( 'thisFile' )( ve );
+		module.exports = createClass;
+	}
+} ( function ( ve ) {
+
 /*!
  * VisualEditor DataModel Change class.
  *
@@ -426,3 +436,5 @@ ve.dm.Change.prototype.serialize = function ( preserveStoreValues ) {
 		}
 	};
 };
+
+} ) );
