a4c8f2bab90be7fc4c0c13c4de1f8fe192b4a420
David Chan <david@troi.org>
2016-08-18 12:14:51 -0700

    WIP: Rebaser
    Change-Id: I068447d8155b0732c08d642357dab07cfca5aa2a

--- src/dm/ve.dm.IndexValueStore.js
+++ src/dm/ve.dm.IndexValueStore.js
@@ -1,3 +1,13 @@
+( function ( createClass ) {
+	if ( typeof window !== 'undefined' ) {
+		// Browser
+		createClass( window.ve );
+	} else {
+		// Node. ve={dm:{}}; require( 'thisFile' )( ve );
+		module.exports = createClass;
+	}
+} ( function ( ve ) {
+
 /*!
  * VisualEditor IndexValueStore class.
  *
@@ -264,3 +274,5 @@ ve.dm.IndexValueStore.prototype.filter = function ( transactions ) {
 	}
 	return store;
 };
+
+} ) );
