82593e7d233fce82352d475a928704149f0b2d65
David Chan <david@troi.org>
2016-08-17 19:09:40 -0700

    Method to rebase a transaction onto another
    Change-Id: I2c83056b24e45edb35aceb78f8bf066866e6563f

--- tests/dm/ve.dm.Transaction.test.js
+++ tests/dm/ve.dm.Transaction.test.js
@@ -2556,3 +2556,81 @@ QUnit.test( 'toJSON/build with operations', function ( assert ) {
 
 	}
 } );
+
+QUnit.test( 'rebaseOnto', function ( assert ) {
+	var doc = ve.dm.example.createExampleDocument(),
+		bold = ve.dm.example.createAnnotation( ve.dm.example.bold ),
+		replace12 = ve.dm.Transaction.newFromReplacement( doc, new ve.Range( 1, 2 ), [ 'f', 'o', 'o' ] ),
+		replace23 = ve.dm.Transaction.newFromReplacement( doc, new ve.Range( 2, 3 ), [ 'b', 'a', 'r' ] ),
+		replace13 = ve.dm.Transaction.newFromReplacement( doc, new ve.Range( 1, 3 ), [ 'b', 'a', 'z' ] ),
+		replace24 = ve.dm.Transaction.newFromReplacement( doc, new ve.Range( 2, 4 ), [ 'q', 'u', 'x' ] ),
+		annotate12 = ve.dm.Transaction.newFromAnnotation( doc, new ve.Range( 1, 2 ), 'set', bold );
+
+	assert.deepEqual( replace23.rebaseOnto( replace12 ).operations, [
+		{
+			type: 'retain',
+			length: 4
+		},
+		{
+			type: 'replace',
+			remove: [ [ 'b', [ ve.dm.example.boldIndex ] ] ],
+			insert: [ 'b', 'a', 'r' ],
+			insertedDataOffset: 0,
+			insertedDataLength: 3
+		},
+		{
+			type: 'retain',
+			length: 60
+		}
+	], 'Rebase onto upwind replace' );
+
+	assert.deepEqual( replace12.rebaseOnto( replace23 ).operations, [
+		{
+			type: 'retain',
+			length: 1
+		},
+		{
+			type: 'replace',
+			remove: [ 'a' ],
+			insert: [ 'f', 'o', 'o' ],
+			insertedDataOffset: 0,
+			insertedDataLength: 3
+		},
+		{
+			type: 'retain',
+			length: 63
+		}
+	], 'Rebase onto downwind replace' );
+
+	assert.deepEqual( replace12.rebaseOnto( replace23 ).operations, [
+		{
+			type: 'retain',
+			length: 1
+		},
+		{
+			type: 'replace',
+			remove: [ 'a' ],
+			insert: [ 'f', 'o', 'o' ],
+			insertedDataOffset: 0,
+			insertedDataLength: 3
+		},
+		{
+			type: 'retain',
+			length: 63
+		}
+	], 'Rebase onto downwind replace' );
+
+	assert.deepEqual( replace24.rebaseOnto( annotate12 ).operations, replace24.operations, 'Rebase onto upwind annotate' );
+
+	assert.deepEqual( replace12.rebaseOnto( replace13 ), null, 'Rebase conflict with surrounding replace' );
+	assert.deepEqual( replace13.rebaseOnto( replace12 ), null, 'Rebase conflict with surrounded replace' );
+	assert.deepEqual( replace13.rebaseOnto( replace24 ), null, 'Rebase conflict with overlapping downwind replace' );
+	assert.deepEqual( replace24.rebaseOnto( replace13 ), null, 'Rebase conflict with overlapping upwind replace' );
+	assert.deepEqual( replace12.rebaseOnto( annotate12 ), null, 'Rebase conflict with overlapping annotate' );
+
+	assert.deepEqual(
+		replace12.rebaseOnto( replace23 ).rebaseOnto( replace23.reversed() ).operations,
+		replace12.operations,
+		'Inverse rebase'
+	);
+} );
