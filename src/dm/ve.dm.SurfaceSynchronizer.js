5042025e04b51d655ee1569a55a770ba8de7c650
Roan Kattouw <roan.kattouw@gmail.com>
2016-09-10 00:49:28 -0700

    [WIP] sockets
    Change-Id: I724f222dd26b217d7cbb4095a2374faa7ed0718c

--- src/dm/ve.dm.SurfaceSynchronizer.js
+++ src/dm/ve.dm.SurfaceSynchronizer.js
@@ -26,6 +26,8 @@ ve.dm.SurfaceSynchronizer = function VeDmSurfaceSynchronizer( surface ) {
 	this.userSelections = {};
 	this.syncDebounced = ve.debounce( this.sync.bind( this ), 5000 );
 	this.documentId = 'EXAMPLE';
+	this.submittedChange = null;
+	this.pendingChange = null;
 	this.authorId = Math.random();
 	document.body.insertBefore( document.createTextNode( this.authorId ), document.body.firstChild );
 	this.prevSelection = null;
@@ -33,16 +35,23 @@ ve.dm.SurfaceSynchronizer = function VeDmSurfaceSynchronizer( surface ) {
 	if ( window.QUnit ) {
 		return;
 	}
+
+	this.socket = io();
+	this.socket.on( 'newChange', this.onNewChange.bind( this ) );
+	this.socket.on( 'rejectedChange', this.onRejectedChange.bind( this ) );
+
 	// Events
 	this.doc.connect( this, {
 		transact: 'onDocumentTransact'
 	} );
-	this.doc.connect( this, {
+
+	this.submitChangeDebounced = ve.debounce( this.submitChange.bind( this ), 3000 );
+	/*this.doc.connect( this, {
 		select: this.syncDebounced
 	} );
 
 	this.syncInterval = 5000;
-	this.sync();
+	this.sync();*/
 };
 
 /* Inheritance */
@@ -60,8 +69,211 @@ ve.dm.SurfaceSynchronizer.prototype.onDocumentTransact = function ( tx ) {
 	for ( userId in this.userSelections ) {
 		this.userSelections[ userId ].selection = this.userSelections[ userId ].selection.translateByTransaction( tx );
 	}
-	this.syncDebounced();
+	//this.syncDebounced();
+	this.submitChangeDebounced();
 };
+
+ve.dm.SurfaceSynchronizer.prototype.submitChange = function () {
+	if ( this.submittedChange ) {
+		// We're already submitting a change. Once the server responds to us, we'll rebase the
+		// unsent local changes and submit them then.
+		return;
+	}
+
+	var documentModel = this.getSurface().getDocument(),
+		sync = this,
+		change = documentModel.getChangeSince( this.transactionCommitLength, this.storeCommitLength );
+	if ( change.transactions.length === 0 ) {
+		return;
+	}
+	this.submittedChange = change;
+	this.pendingChange = change.end();
+	this.socket.emit( 'submitChange', {
+		doc: this.documentId,
+		author: this.authorId,
+		change: change.serialize()
+	} );
+	console.log( this.authorId, 'submitChange', change.serialize() );
+};
+
+ve.dm.SurfaceSynchronizer.prototype.onNewChange = function ( data ) {
+	console.log( this.authorId, 'newChange', data );
+
+	var pendingOnSubmitted, submittedOnPending, unsent, rebases, unsentRebased, incoming, canonicalHistory,
+		change = ve.dm.Change.static.deserialize( data.change );
+
+	if ( this.submittedChange ) {
+		if ( data.author === this.authorId ) {
+			// Server accepted our change
+			console.log( this.authorId, 'server accepted our change', change );
+			// Let p = pendingChange and s = submittedChange.
+			// The server has sent us submittedOnPending = s/p. Compute pendingOnSubmitted = p\s.
+			submittedOnPending = change;
+			pendingOnSubmitted = ve.dm.Change.static.rebaseChanges( this.pendingChange, this.submittedChange )[ 0 ];
+			// TODO maybe sanity-check/assert that change is equal to the [1] of this rebase?
+			canonicalHistory = this.pendingChange.concat( submittedOnPending ); // p + s/p
+
+			// Get the unsent local changes (= u)
+			unsent = this.doc.getChangeSince(
+				this.submittedChange.transactionStart + this.submittedChange.transactions.length,
+				this.submittedChange.storeStart + this.submittedChange.store.getLength()
+			);
+			// Compute the rebases of u over p\s
+			rebases = ve.dm.Change.static.rebaseChanges( pendingOnSubmitted, unsent );
+			incoming = rebases[ 0 ]; // (p\s) \ u
+			unsentRebased = rebases[ 1 ]; // u / (p\s)
+			if ( unsentRebased ) {
+				// Apply incoming (= (p\s) \ u) to the existing state, obtaining s + u + (p\s)\u,
+				// but rewrite the history to p + s/p + u/(p\s). These two are equivalent:
+				//     u + (p\s)\u = p\s + u/(p\s)       (rebase axiom)
+				// s + u + (p\s)\u = s + p\s + u/(p\s)   (pre-concat s)
+				// s + u + (p\s)\u = p + s/p + u/(p\s)   (s + p\s = p + s/p by rebase axiom)
+
+				console.log( this.authorId, 'rebase of unsent changes succeeded' );
+				// Apply incoming
+				incoming.applyTo( this.surface );
+
+				// Rewind history to before s
+				this.doc.completeHistory.splice( this.submittedChange.transactionStart );
+				this.store.truncate( this.submittedChange.storeStart );
+
+				// Add p + s/p to the history
+				this.store.merge( canonicalHistory.store );
+				canonicalHistory.addToHistory( this.doc.completeHistory );
+
+				// Mark this position as the synced position
+				this.transactionCommitLength = this.doc.completeHistory.length;
+				this.storeCommitLength = this.store.getLength();
+
+				// Add u/(p\s) to the history
+				this.store.merge( unsentRebased.store );
+				unsentRebased.addToHistory( this.doc.completeHistory );
+				if ( unsentRebased.transactions.length > 0 ) {
+					// Submit the unsent changes now
+					this.submitChange();
+				}
+			} else {
+				// The unsent changes conflict with the pending changes, so discard the unsent changes.
+				// Our current state is s + u, so we will apply -u + p\s which gets us to s + p\s.
+				// We'll rewrite the history to p + s/p, which is equivalent.
+
+				// TODO maybe reduce code duplication with the if case
+				// TODO maybe do that by creating helper functions for "rewind", "add to history", "mark synced"
+
+				console.log( this.authorId, 'rebase of unsent changes failed' );
+
+				// Apply -u
+				unsent.reversed().applyTo( this.surface );
+
+				// Rewind history to before s
+				this.doc.completeHistory.splice( this.submittedChange.transactionStart );
+				this.store.truncate( this.submittedChange.storeStart );
+
+				// Add p + s/p to the history
+				this.store.merge( canonicalHistory.store );
+				canonicalHistory.addToHistory( this.doc.completeHistory );
+
+				// Mark this position as the synced position
+				this.transactionCommitLength = this.doc.completeHistory.length;
+				this.storeCommitLength = this.store.getLength();
+			}
+			this.submittedChange = null;
+			this.pendingChange = null;
+		} else {
+			// Someone else made a change while we have a change in flight
+			// Queue it up and apply it when our change is accepted or rejected
+			console.log( this.authorId, 'queueing change', change );
+			if ( this.pendingChange ) {
+				this.pendingChange = this.pendingChange.concat( change );
+			} else {
+				this.pendingChange = chuange;
+			}
+		}
+	} else {
+		if ( data.author === this.authorId ) {
+			// WTF
+			console.warn( 'Server unexpectedly sent us our own change', change );
+		} else {
+			console.log( this.authorId, 'applying change', change );
+			unsent = this.doc.getChangeSince( this.transactionCommitLength, this.storeCommitLength );
+			rebases = ve.dm.Change.static.rebaseChanges( change, unsent );
+			incoming = rebases[ 0 ]; // c\u
+			unsentRebased = rebases[ 1 ]; // u/c
+			if ( unsentRebased ) {
+				console.log( this.authorId, 'rebasing onto unsent change', unsent );
+				// Apply c\u to the current state, but rewrite the history to c + u/c
+				incoming.applyTo( this.surface );
+
+				// Rewind history to before u
+				this.doc.completeHistory.splice( unsent.transactionStart );
+				this.store.truncate( unsent.storeStart );
+
+				// Add c to the history
+				this.store.merge( change.store );
+				change.addToHistory( this.doc.completeHistory );
+
+				// Mark this position as the synced position
+				this.transactionCommitLength = this.doc.completeHistory.length;
+				this.storeCommitLength = this.store.getLength();
+
+				// Add u/c to the history
+				this.store.merge( unsentRebased.store );
+				unsentRebased.addToHistory( this.doc.completeHistory );
+
+				if ( unsentRebased.transactions.length > 0 ) {
+					// Submit the unsent changes now
+					this.submitChange();
+				}
+			} else {
+				// The unsent changes conflict with the new changes, so discard the unsent changes
+				console.log( this.authorId, 'conflict, discarding unsent change', unsent );
+
+				// Apply -u
+				unsent.reversed().applyTo( this.surface );
+
+				// Rewind history to before u
+				this.doc.completeHistory.splice( unsent.transactionStart );
+				this.store.truncate( unsent.storeStart );
+
+				// Add c to the history
+				this.store.merge( change.store );
+				change.addToHistory( this.doc.completeHistory );
+
+				// Mark this position as the synced position
+				this.transactionCommitLength = this.doc.completeHistory.length;
+				this.storeCommitLength = this.store.getLength();
+			}
+		}
+	}
+};
+
+ve.dm.SurfaceSynchronizer.prototype.onRejectedChange = function ( data ) {
+	// Undo our local changes and remove them from the history
+	var localChange = this.doc.getChangeSince(
+		this.submittedChange.transactionStart,
+		this.submittedChange.storeStart
+	);
+	console.log( this.authorId, 'conflict, undoing local change', localChange );
+	localChange.reversed().applyTo( this.surface );
+	this.doc.completeHistory.splice( this.submittedChange.transactionStart );
+	this.doc.store.truncate( this.submittedChange.storeStart );
+
+	// Apply pending changes
+	if ( this.pendingChange ) {
+		console.log( this.authorId, 'applying pending change', this.pendingChange );
+		this.pendingChange.applyTo( this.surface );
+		this.pendingChange = null;
+	}
+
+	this.transactionCommitLength = this.doc.completeHistory.length;
+	this.storeCommitLength = this.store.getLength();
+	this.submittedChange = null;
+};
+
+
+
+// Old stuff below here
+// --------------------------------------------------------------------------
 
 ve.dm.SurfaceSynchronizer.prototype.sync = function () {
 	var selection = JSON.stringify( this.getSurface().getSelection() ),
