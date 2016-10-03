f90c2feebc91ad2b51cac14a282765f6c65a4e52
David Lynch <dlynch@wikimedia.org>
2016-10-03 11:46:29 -0500

    ce.CommentNode: inline preview of comment
    Bug: T147089
    Change-Id: I23eea7593ef2531ae91fa533732cfb5c36b9a4b9

--- src/ce/nodes/ve.ce.CommentNode.js
+++ src/ce/nodes/ve.ce.CommentNode.js
@@ -22,6 +22,9 @@ ve.ce.CommentNode = function VeCeCommentNode( model, config ) {
 	// Mixin constructors
 	ve.ce.FocusableNode.call( this, this.$element, config );
 
+	// Events
+	this.model.connect( this, { attributeChange: 'onAttributeChange' } );
+
 	// DOM changes
 	this.$element.addClass( 've-ce-commentNode' );
 };
@@ -39,6 +42,8 @@ ve.ce.CommentNode.static.primaryCommandName = 'comment';
 
 ve.ce.CommentNode.static.iconWhenInvisible = 'notice';
 
+ve.ce.CommentNode.static.maxCommentLength = 20;
+
 /* Static Methods */
 
 /**
@@ -48,11 +53,51 @@ ve.ce.CommentNode.static.getDescription = function ( model ) {
 	return model.getAttribute( 'text' );
 };
 
+ve.ce.CommentNode.static.getTextPreview = function ( text ) {
+	// The text preview is a trimmed down version of the actual comment. This
+	// means that we strip whitespace, replace newlines, and truncate to a
+	// fairly short length. The goal is to provide a fair representation of
+	// typical short comments, and enough context for long comments that the
+	// user can tell whether they want to see the full view by focusing the
+	// node / hovering.
+	text = text.trim().replace( /\n+/, ' ' );
+	if ( text.length > this.maxCommentLength ) {
+		text = ve.graphemeSafeSubstring( text, 0, this.maxCommentLength ) + '...';
+	}
+	return text;
+};
+
 /**
- * @inheritdoc ve.ce.FocusableNode
+ * Update the rendering of the 'text' attribute
+ * when it changes in the model.
+ *
+ * @method
+ * @param {string} key Attribute key
+ * @param {string} from Old value
+ * @param {string} to New value
  */
-ve.ce.CommentNode.prototype.hasRendering = function () {
-	return false;
+ve.ce.CommentNode.prototype.onAttributeChange = function ( key, from, to ) {
+	switch ( key ) {
+		case 'text':
+			this.icon.setLabel( this.constructor.static.getTextPreview( to ) );
+			break;
+	}
+};
+
+/* Methods */
+
+/**
+ * @inheritdoc
+ */
+ve.ce.CommentNode.prototype.createInvisibleIcon = function () {
+	var icon = new OO.ui.ButtonWidget( {
+		classes: [ 've-ce-focusableNode-invisibleIcon' ],
+		framed: false,
+		icon: this.constructor.static.iconWhenInvisible,
+		label: this.constructor.static.getTextPreview( this.getModel().getAttribute( 'text' ) )
+	} );
+	this.icon = icon;
+	return icon.$element;
 };
 
 /* Registration */
