1ae92c6f72fa03a167d2817075384b67e7e377f9
David Lynch <dlynch@wikimedia.org>
2016-10-03 11:46:29 -0500

    ce.CommentNode: inline preview of comment
    Bug: T147089
    Change-Id: I23eea7593ef2531ae91fa533732cfb5c36b9a4b9

--- src/ce/nodes/ve.ce.CommentNode.js
+++ src/ce/nodes/ve.ce.CommentNode.js
@@ -22,7 +22,15 @@ ve.ce.CommentNode = function VeCeCommentNode( model, config ) {
 	// Mixin constructors
 	ve.ce.FocusableNode.call( this, this.$element, config );
 
+	this.$element.data( 'view', this );
+
+	// Events
+	this.connect( this, { setup: 'onSetup' } );
+	this.model.connect( this, { attributeChange: 'onAttributeChange' } );
+
 	// DOM changes
+	this.$text = $( '<span>' ).addClass( 've-ce-commentNode-text' );
+	this.$element.append( this.$text );
 	this.$element.addClass( 've-ce-commentNode' );
 };
 
@@ -39,6 +47,8 @@ ve.ce.CommentNode.static.primaryCommandName = 'comment';
 
 ve.ce.CommentNode.static.iconWhenInvisible = 'notice';
 
+ve.ce.CommentNode.static.maxCommentLength = 20;
+
 /* Static Methods */
 
 /**
@@ -49,10 +59,44 @@ ve.ce.CommentNode.static.getDescription = function ( model ) {
 };
 
 /**
- * @inheritdoc ve.ce.FocusableNode
+ * Update the rendering of the 'text' attribute
+ * when it changes in the model.
+ *
+ * @method
+ * @param {string} key Attribute key
+ * @param {string} from Old value
+ * @param {string} to New value
  */
-ve.ce.CommentNode.prototype.hasRendering = function () {
-	return false;
+ve.ce.CommentNode.prototype.onAttributeChange = function ( key, from, to ) {
+	switch ( key ) {
+		case 'text':
+			this.updateText( to );
+			break;
+	}
+};
+
+/**
+ * Handle node setup.
+ *
+ * @method
+ */
+ve.ce.CommentNode.prototype.onSetup = function ( ) {
+	this.updateText( this.model.getAttribute( 'text' ) );
+};
+
+ve.ce.CommentNode.prototype.updateText = function ( text ) {
+	// The text preview is a trimmed down version of the actual comment. This
+	// means that we strip whitespace, replace newlines, and truncate to a
+	// fairly short length. The goal is to provide a fair representation of
+	// typical short comments, and enough context for long comments that the
+	// user can tell whether they want to see the full view by focusing the
+	// node / hovering.
+	text = text.trim().replace( /\n+/, ' ' );
+	if ( text.length > this.constructor.static.maxCommentLength ) {
+		text = ve.graphemeSafeSubstring( text, 0, this.constructor.static.maxCommentLength ) + '...';
+	}
+	this.$text.text( text );
+	this.$text.toggle( text.length > 0 );
 };
 
 /* Registration */
