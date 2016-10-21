0e74d9ae8ed6e24cdf17e08e98ddce68fa055c6c
David Chan <david@troi.org>
2016-10-20 18:57:01 -0700

    Human-readable transaction summary
    Change-Id: I6920514df81c304a02a672a2eabffb91a8f1e5e7

--- src/dm/ve.dm.Transaction.js
+++ src/dm/ve.dm.Transaction.js
@@ -913,6 +913,52 @@ ve.dm.Transaction.prototype.toJSON = function () {
 };
 
 /**
+ * Get a human-readable summary
+ *
+ * @return {string} Human-readable summary
+ */
+ve.dm.Transaction.prototype.summarize = function () {
+	var annotations = 0;
+	function summarizeItems( items ) {
+		return '\'' + items.map( function ( item ) {
+			if ( item.type ) {
+				return '<' + item.type + '>';
+			} else if ( Array.isArray( item ) ) {
+				return item[ 0 ];
+			} else if ( typeof item === 'string' ) {
+				return item;
+			} else {
+				throw new Error( 'Unknown item type: ' + item );
+			}
+		} ).join( '' ) + '\'';
+	}
+	return '(' + this.operations.map( function ( op ) {
+		if ( op.type === 'retain' ) {
+			return ( annotations ? 'annotate ' : 'retain ' ) + op.length;
+		} else if ( op.type === 'replace' ) {
+			if ( op.remove.length === 0 ) {
+				return 'insert ' + summarizeItems( op.insert );
+			} else if ( op.insert.length === 0 ) {
+				return 'remove ' + summarizeItems( op.remove );
+			} else {
+				return 'replace ' + summarizeItems( op.remove ) +
+					' -> ' + summarizeItems( op.insert );
+			}
+		} else if ( op.type === 'attribute' ) {
+			return 'attribute';
+		} else if ( op.type === 'annotate' ) {
+			annotations += op.bias === 'start' ? 1 : -1;
+			return 'annotate';
+		} else if ( op.type.endsWith( 'Metadata' ) ) {
+			// We don't care much because we're deprecating this
+			return 'metadata';
+		} else {
+			throw new Error( 'Unknown op type: ' + op.type );
+		}
+	} ).join( ', ' ) + ')';
+};
+
+/**
  * Create a clone of this transaction.
  *
  * The returned transaction will be exactly the same as this one, except that its 'applied' flag
