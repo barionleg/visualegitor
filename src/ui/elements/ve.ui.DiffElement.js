4aafea8edb1acea4f050f7a535e16dccda3f7479
Thalia <thalia.e.chan@googlemail.com>
2016-11-02 21:34:46 +0000

    Fix splice typo
    Bug: T149537
    Bug: T149528
    Change-Id: If8d137aeaf4e0097ca4643feaeed381ded65d51b

--- src/ui/elements/ve.ui.DiffElement.js
+++ src/ui/elements/ve.ui.DiffElement.js
@@ -313,8 +313,10 @@ ve.ui.DiffElement.prototype.getChangedNodeHtml = function ( oldNodeIndex, move )
 				insertIndex = newPreviousNodeRange.from - nodeRange.from;
 			}
 			// Splice in the subnode's annotated data just after the new node's open tag
-			ve.batchSplice( nodeData, 0, insertIndex, outerNodeData );
+			ve.batchSplice( nodeData, insertIndex, 0, outerNodeData );
 		}
+
+		alreadyProcessed[ action ].push( nodeIndex );
 
 	}
 
