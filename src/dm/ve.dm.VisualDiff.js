3c86e921f21a612e9b0ca49a11d3d8340abfee5f
Ed Sanders <esanders@wikimedia.org>
2017-03-03 15:38:07 +0000

    DiffElement: Ensure del/ins tags encapsulate annotation changes
    This is more semantically correct, and means that our 'del + ins'
    CSS selectors still work.
    
    Change-Id: I13fb38e4e9943be328ba6dc85c124e74de451acd

--- src/dm/ve.dm.VisualDiff.js
+++ src/dm/ve.dm.VisualDiff.js
@@ -497,12 +497,10 @@ ve.dm.VisualDiff.prototype.getDocChildDiff = function ( oldDocChild, newDocChild
 				// There is no content change
 				diffInfo[ i ] = {
 					typeChange: oldNode.type !== newNode.type,
-					attributeChange: !ve.compare( oldNode.getAttributes(), newNode.getAttributes() ) ?
-					{
+					attributeChange: !ve.compare( oldNode.getAttributes(), newNode.getAttributes() ) ? {
 						oldAttributes: oldNode.getAttributes(),
 						newAttributes: newNode.getAttributes()
-					} :
-					false
+					} : false
 				};
 				continue;
 
@@ -536,12 +534,10 @@ ve.dm.VisualDiff.prototype.getDocChildDiff = function ( oldDocChild, newDocChild
 				diffInfo[ i ] = {
 					linearDiff: linearDiff,
 					typeChange: oldNode.type !== newNode.type,
-					attributeChange: !ve.compare( oldNode.getAttributes(), newNode.getAttributes() ) ?
-					{
+					attributeChange: !ve.compare( oldNode.getAttributes(), newNode.getAttributes() ) ? {
 						oldAttributes: oldNode.getAttributes(),
 						newAttributes: newNode.getAttributes()
-					} :
-					false
+					} : false
 				};
 
 				if ( linearDiff ) {
