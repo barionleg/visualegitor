dda963126b7d3ff1493fce9b7cec0093b165c1a4
Ed Sanders <esanders@wikimedia.org>
2017-03-03 15:38:07 +0000

    DiffElement: Ensure del/ins tags encapsulate annotation changes
    This is more semantically correct, and means that our 'del + ins'
    CSS selectors still work.
    
    Change-Id: I13fb38e4e9943be328ba6dc85c124e74de451acd

--- src/ui/elements/ve.ui.DiffElement.js
+++ src/ui/elements/ve.ui.DiffElement.js
@@ -471,7 +471,7 @@ ve.ui.DiffElement.prototype.addAttributesToNode = function ( nodeData, nodeDoc, 
 ve.ui.DiffElement.prototype.annotateNode = function ( linearDiff ) {
 	var i, ilen, range, type, typeAsString, annType, domElementType,
 		start = 0, // The starting index for a range for building an annotation
-		end, transaction, annotatedLinearDiff,
+		end, annotations, annotatedLinearDiff,
 		domElement, domElements, originalDomElementsIndex,
 		diffDoc, diffDocData;
 
@@ -519,17 +519,35 @@ ve.ui.DiffElement.prototype.annotateNode = function ( linearDiff ) {
 					domElements,
 					domElements.map( ve.getNodeHtml ).join( '' )
 				);
-				transaction = ve.dm.TransactionBuilder.static.newFromAnnotation(
-					diffDoc, range, 'set',
+				annotations = diffDoc.data.getAnnotationsFromRange( range );
+				/* eslint-disable no-loop-func */
+				// Remove existing annotations so the change annotation can be applied as the outer annotation
+				annotations.get().forEach( function ( ann ) {
+					diffDoc.commit(
+						ve.dm.TransactionBuilder.static.newFromAnnotation(
+							diffDoc, range, 'clear', ann
+						)
+					);
+				} );
+				// Make the del/ins the outer annotation before re-applying everything
+				annotations.add(
 					ve.dm.annotationFactory.create(
 						annType,
 						{
 							type: annType,
 							originalDomElementsIndex: originalDomElementsIndex
 						}
-					)
+					), 0
 				);
-				diffDoc.commit( transaction );
+				// Re-apply all annotations
+				annotations.get().forEach( function ( ann ) {
+					diffDoc.commit(
+						ve.dm.TransactionBuilder.static.newFromAnnotation(
+							diffDoc, range, 'set', ann
+						)
+					);
+				} );
+				/* eslint-enable no-loop-func */
 			}
 		}
 		start = end;
