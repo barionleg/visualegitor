f7a70a3ab7a88f038823577dee802f5d6454ba5b
Ed Sanders <esanders@wikimedia.org>
2018-02-04 13:50:03 +0000

    Standardise origDoc property as orignalDocument
    Also pass into DocumentSlice constructor, and use to
    expose originalDoc's internal list in converter.
    
    Change-Id: Iee635eaf42e3780da4e46e3c0f12417aa331655b

--- src/dm/ve.dm.DocumentSlice.js
+++ src/dm/ve.dm.DocumentSlice.js
@@ -15,10 +15,11 @@
  * @param {ve.dm.InternalList} [internalList]
  * @param {ve.Range} [originalRange] Range of original data
  * @param {ve.Range} [balancedRange] Range of balanced data
+ * @param {ve.dm.Document} [originalDocument]
  */
-ve.dm.DocumentSlice = function VeDmDocumentSlice( data, htmlDocument, internalList, originalRange, balancedRange ) {
+ve.dm.DocumentSlice = function VeDmDocumentSlice( data, htmlDocument, internalList, originalRange, balancedRange, originalDocument ) {
 	// Parent constructor
-	ve.dm.DocumentSlice.super.call( this, data, htmlDocument, internalList );
+	ve.dm.DocumentSlice.super.call( this, data, htmlDocument, undefined, internalList, undefined, undefined, undefined, originalDocument );
 
 	this.originalRange = originalRange;
 	this.balancedRange = balancedRange;
@@ -36,4 +37,8 @@ ve.dm.DocumentSlice.prototype.getOriginalData = function () {
 
 ve.dm.DocumentSlice.prototype.getBalancedData = function () {
 	return this.getData( this.balancedRange );
+};
+
+ve.dm.DocumentSlice.prototype.getOriginalDocument = function () {
+	return this.originalDocument;
 };
