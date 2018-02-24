89b00fe7e90215b588e51353377796a1dcb36937
Thalia <thalia.e.chan@googlemail.com>
2018-02-24 21:19:30 +0000

    WIP show timeout message if diff times out
    Change-Id: I91b46f679032b29f3da9d9bd8384a704cfd2affa

--- src/dm/ve.dm.VisualDiff.js
+++ src/dm/ve.dm.VisualDiff.js
@@ -29,7 +29,9 @@ ve.dm.VisualDiff = function VeDmVisualDiff( oldDoc, newDoc, timeout ) {
 	this.treeDiffer = treeDiffer;
 	// eslint-disable-next-line camelcase,new-cap
 	this.linearDiffer = new ve.DiffMatchPatch( this.oldDoc.getStore(), this.newDoc.getStore() );
+
 	this.endTime = new Date().getTime() + ( timeout || 1000 );
+	this.timedOut = false;
 
 	this.freezeInternalListIndices( this.oldDoc );
 	this.freezeInternalListIndices( this.newDoc );
@@ -435,6 +437,7 @@ ve.dm.VisualDiff.prototype.getDocChildDiff = function ( oldDocChild, newDocChild
 	transactions = new this.treeDiffer.Differ( oldDocChildTree, newDocChildTree ).transactions;
 	if ( transactions === null ) {
 		// Tree diff timed out
+		this.timedOut = true;
 		return false;
 	}
 
@@ -501,6 +504,7 @@ ve.dm.VisualDiff.prototype.getDocChildDiff = function ( oldDocChild, newDocChild
 						{ keepOldText: false }
 					);
 				} else {
+					this.timedOut = true;
 					linearDiff = null;
 					removeLength += oldNode.getLength();
 					insertLength += newNode.getLength();
@@ -553,7 +557,7 @@ ve.dm.VisualDiff.prototype.getDocChildDiff = function ( oldDocChild, newDocChild
 
 	}
 
-	// Only return the diff if enough content has changed
+	// Only return the diff if a high enough proportion of the content has been kept
 	if ( keepLength < threshold * diffLength ) {
 		return false;
 	}
