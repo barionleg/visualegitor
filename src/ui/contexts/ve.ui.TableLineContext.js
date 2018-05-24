eb4c2d712a158f38a2bf20b334b9ccd67fe9e6cf
Ed Sanders <esanders@wikimedia.org>
2018-05-24 15:30:17 +0100

    VWIP Table context to dropdown
    Bug: T178387
    Change-Id: I5f9eae4b9fbe7dd881aba628060994e5019241dd

--- src/ui/contexts/ve.ui.TableLineContext.js
+++ src/ui/contexts/ve.ui.TableLineContext.js
@@ -14,48 +14,87 @@
  *
  * @constructor
  * @param {ve.ce.TableNode} tableNode
- * @param {string} itemGroup Tool group to use, 'col' or 'row'
+ * @param {string} mode Mode to use, 'col' or 'row'
  * @param {Object} [config] Configuration options
  */
-ve.ui.TableLineContext = function VeUiTableLineContext( tableNode, itemGroup, config ) {
-	config = config || {};
+ve.ui.TableLineContext = function VeUiTableLineContext( tableNode, mode, config ) {
+	var widget = this,
+		surfaceModel = tableNode.surface.getSurface().getModel();
 
+	// tableNode.surface.getSurface()
 	// Parent constructor
-	ve.ui.TableLineContext.super.call( this, tableNode.surface.getSurface(), config );
+	ve.ui.TableLineContext.super.call( this, ve.extendObject( {
+		icon: mode === 'col' ? 'expand' : 'next',
+		indicator: '',
+		$overlay: true,
+		menu: {
+			width: 'auto',
+			horizontalPosition: mode === 'col' ? 'center' : 'after',
+			verticalPosition: mode === 'col' ? 'below' : 'top'
+		}
+	}, config ) );
 
 	// Properties
 	this.tableNode = tableNode;
-	this.itemGroup = itemGroup;
-	this.icon = new OO.ui.IconWidget( {
-		icon: itemGroup === 'col' ? 'expand' : 'next'
-	} );
-	this.popup = new OO.ui.PopupWidget( {
-		classes: [ 've-ui-tableLineContext-menu' ],
-		$container: this.surface.$element,
-		$floatableContainer: this.icon.$element,
-		position: itemGroup === 'col' ? 'below' : 'after',
-		width: 180
-	} );
+	this.mode = mode;
+	// this.popup = new OO.ui.PopupWidget( {
+	// 	classes: [ 've-ui-tableLineContext-menu' ],
+	// 	$container: this.surface.$element,
+	// 	$floatableContainer: this.icon.$element,
+	// 	position: itemGroup === 'col' ? 'below' : 'after',
+	// 	width: 180
+	// } );
 
-	// Events
-	this.icon.$element.on( 'mousedown', this.onIconMouseDown.bind( this ) );
-	this.onDocumentMouseDownHandler = this.onDocumentMouseDown.bind( this );
+	var sides = [ 'before', 'after' ],
+		actions = [ 'insert', 'move', 'delete' ],
+		sideNames = { before: 'Before', after: 'After' },
+		actionNames = { insert: 'Add', move: 'Move' },
+		modeName = { row: 'Row', col: 'Column' }[ mode ];
+
+	actions.forEach( function ( action ) {
+		var actionName = actionNames[ action ];
+
+		if ( action !== 'delete' ) {
+			sides.forEach( function ( side ) {
+				var sideName = sideNames[ side ];
+
+				widget.menu.addItems( [
+					new ve.ui.TableOptionWidget( surfaceModel, action, mode, side, {
+						data: {
+							commandName: action + modeName + sideName,
+							action: action,
+							mode: mode,
+							side: side
+						}
+					} )
+				] );
+			} );
+		} else {
+			widget.menu.addItems( [
+				new ve.ui.TableOptionWidget( surfaceModel, action, mode, null, {
+					data: {
+						commandName: action + modeName,
+						action: action,
+						mode: mode
+					}
+				} )
+			] );
+		}
+	} );
 
 	// Initialization
-	this.popup.$body.append( this.$group );
 	// The following classes can be used here:
 	// * ve-ui-tableLineContext-col
 	// * ve-ui-tableLineContext-row
 	this.$element
-		.addClass( 've-ui-tableLineContext ve-ui-tableLineContext-' + itemGroup )
-		.append( this.icon.$element, this.popup.$element );
+		.addClass( 've-ui-tableLineContext ve-ui-tableLineContext-' + mode );
 	// Visibility is handled by the table overlay
-	this.toggle( true );
+	// this.toggle( true );
 };
 
 /* Inheritance */
 
-OO.inheritClass( ve.ui.TableLineContext, ve.ui.Context );
+OO.inheritClass( ve.ui.TableLineContext, OO.ui.DropdownWidget );
 
 /* Static Properties */
 
@@ -69,21 +108,12 @@ ve.ui.TableLineContext.static.groups = {
 /**
  * @inheritdoc
  */
-ve.ui.TableLineContext.prototype.getRelatedSources = function () {
-	var i, l,
-		items = this.constructor.static.groups[ this.itemGroup ];
-
-	if ( !this.relatedSources ) {
-		this.relatedSources = [];
-
-		for ( i = 0, l = items.length; i < l; i++ ) {
-			this.relatedSources.push( {
-				type: 'item',
-				name: items[ i ]
-			} );
-		}
+ve.ui.TableLineContext.prototype.onMenuToggle = function ( isVisible ) {
+	if ( isVisible ) {
+		this.menu.items.forEach( function ( item ) {
+			item.update();
+		} );
 	}
-	return this.relatedSources;
 };
 
 /**
@@ -94,61 +124,10 @@ ve.ui.TableLineContext.prototype.onContextItemCommand = function () {
 };
 
 /**
- * Handle mouse down events on the icon
- *
- * @param {jQuery.Event} e Mouse down event
- */
-ve.ui.TableLineContext.prototype.onIconMouseDown = function ( e ) {
-	e.preventDefault();
-	this.toggleMenu();
-};
-
-/**
- * Handle document mouse down events
- *
- * @param {jQuery.Event} e Mouse down event
- */
-ve.ui.TableLineContext.prototype.onDocumentMouseDown = function ( e ) {
-	if ( !$( e.target ).closest( this.$element ).length ) {
-		this.toggleMenu( false );
-	}
-};
-
-/**
  * Handle model select events
  *
  * @param {ve.dm.Selection} selection
  */
 ve.ui.TableLineContext.prototype.onModelSelect = function () {
 	this.toggleMenu();
-};
-
-/**
- * @inheritdoc
- */
-ve.ui.TableLineContext.prototype.toggleMenu = function ( show ) {
-	var dir, surfaceModel, surfaceView;
-
-	surfaceModel = this.surface.getModel();
-	surfaceView = this.surface.getView();
-
-	this.popup.toggle( show );
-	if ( this.popup.isVisible() ) {
-		this.tableNode.setEditing( false );
-		surfaceModel.connect( this, { select: 'onModelSelect' } );
-		surfaceView.$document.on( 'mousedown', this.onDocumentMouseDownHandler );
-		dir = surfaceView.getSelection().getDirection();
-		this.$element
-			.removeClass( 've-ui-dir-block-rtl ve-ui-dir-block-ltr' )
-			.addClass( 've-ui-dir-block-' + dir );
-	} else {
-		surfaceModel.disconnect( this );
-		surfaceView.$document.off( 'mousedown', this.onDocumentMouseDownHandler );
-	}
-
-	// Parent method - call after selection has been possibly modified above
-	ve.ui.TableLineContext.super.prototype.toggleMenu.call( this, show );
-
-	// Update popup positioning after the parent method fills in its contents
-	this.popup.position();
 };
