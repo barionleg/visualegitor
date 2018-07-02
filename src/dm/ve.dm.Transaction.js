2c3077bcac01911bca3c07c48623e98f9f702dd3
David Chan <david@troi.org>
2018-07-02 17:57:57 +0100

    WIP: Squash Changes to use fewer transactions
    TODO: transaction range arguments
    
    Bug: T188026
    Change-Id: Id61c89c03bb2d44c575e1a4fb25145432723f808

--- src/dm/ve.dm.Transaction.js
+++ src/dm/ve.dm.Transaction.js
@@ -121,6 +121,40 @@ ve.dm.Transaction.static.deserialize = function ( data ) {
 /* Methods */
 
 /**
+ * Freeze the transaction's operations with Object.freeze in useful places
+ *
+ * If you use this, you probably want "use strict"; at the top of suspect files
+ */
+ve.dm.Transaction.prototype.debugFreeze = function () {
+	function freezeItem( item ) {
+		if ( Array.isArray( item ) ) {
+			item.forEach( Object.freeze );
+			Object.freeze( item );
+		} else if ( typeof item === 'object' ) {
+			if ( item.attributes ) {
+				Object.freeze( item.attributes );
+			}
+			if ( item.annotations ) {
+				Object.freeze( item.annotations );
+			}
+			Object.freeze( item );
+		}
+		Object.freeze( item );
+	}
+	function freezeOperation( op ) {
+		if ( op.type === 'replace' ) {
+			op.remove.forEach( freezeItem );
+			op.insert.forEach( freezeItem );
+			Object.freeze( op.remove );
+			Object.freeze( op.insert );
+		}
+		Object.freeze( op );
+	}
+	this.operations.forEach( freezeOperation );
+	Object.freeze( this.operations );
+};
+
+/**
  * Serialize the transaction into a JSONable object
  *
  * Values are not necessarily deep copied
@@ -560,10 +594,12 @@ ve.dm.Transaction.prototype.getModifiedRange = function ( doc, includeInternalLi
  * @return {Object} Active range and length change
  * @return {number|undefined} return.start Start offset of the active range
  * @return {number|undefined} return.end End offset of the active range
+ * @return {number|undefined} return.startOpIndex Start operation index of the active range
+ * @return {number|undefined} return.endOpIndex End operation index of the active range
  * @return {number} return.diff Length change the transaction causes
  */
 ve.dm.Transaction.prototype.getActiveRangeAndLengthDiff = function () {
-	var i, len, op, start, end, active,
+	var i, len, op, start, end, startOpIndex, endOpIndex, active,
 		offset = 0,
 		annotations = 0,
 		diff = 0;
@@ -580,6 +616,7 @@ ve.dm.Transaction.prototype.getActiveRangeAndLengthDiff = function () {
 		// Place start marker
 		if ( active && start === undefined ) {
 			start = offset;
+			startOpIndex = i;
 		}
 		// Adjust offset and diff
 		if ( op.type === 'retain' ) {
@@ -592,11 +629,19 @@ ve.dm.Transaction.prototype.getActiveRangeAndLengthDiff = function () {
 		if ( op.type === 'attribute' || op.type === 'replaceMetadata' ) {
 			// Op with length 0 but that effectively modifies 1 position
 			end = offset + 1;
+			endOpIndex = i + 1;
 		} else if ( active ) {
 			end = offset;
+			endOpIndex = i + 1;
 		}
 	}
-	return { start: start, end: end, diff: diff };
+	return {
+		start: start,
+		end: end,
+		startOpIndex: startOpIndex,
+		endOpIndex: endOpIndex,
+		diff: diff
+	};
 };
 
 // TODO: Use adjustRetain to replace ve.dm.TransactionBuilder#pushRetain
@@ -631,4 +676,84 @@ ve.dm.Transaction.prototype.adjustRetain = function ( place, diff ) {
 		throw new Error( 'Negative retain length' );
 	}
 	ops.splice( start ? 0 : ops.length, 0, { type: 'retain', length: diff } );
+};
+
+/**
+ * Split (in place) the retain at the given offset, if any
+ *
+ * Offset cannot be in the interior of a replace operation (i.e. the interior of its removed content).
+ *
+ * @param {number} offset The offset at which to split
+ * @return {number} Index in operations starting at offset
+ * @throws {Error} Offset is in the interior of a replace operation
+ */
+ve.dm.Transaction.prototype.trySplit = function ( offset ) {
+	var i, iLen, op, opLen,
+		n = 0;
+	for ( i = 0, iLen = this.operations.length; i < iLen; i++ ) {
+		op = this.operations[ i ];
+		opLen = ( op.type === 'retain' ? op.length : op.type === 'replace' ? op.remove.length : 0 );
+		if ( n + opLen <= offset ) {
+			n += opLen;
+			continue;
+		}
+		if ( n === offset ) {
+			// At start edge; no need to split
+			return i;
+		}
+		// Else n < offset < n + opLen
+		if ( op.type !== 'retain' ) {
+			throw new Error( 'Cannot split operation of type ' + op.type );
+		}
+		// Split the retain operation
+		op.length -= n + opLen - offset;
+		this.operations.splice( i + 1, 0, { type: 'retain', length: n + opLen - offset } );
+		return i + 1;
+	}
+	if ( n === offset ) {
+		return iLen + 1;
+	}
+	throw new Error( 'Offset beyond end of transaction' );
+};
+
+/**
+ * Unsplit (in place) the two operations around the given index, if possible
+ *
+ * @param {number} index The index at which to unsplit
+ */
+ve.dm.Transaction.prototype.tryUnsplit = function ( index ) {
+	var op1 = this.operations[ index - 1 ],
+		op2 = this.operations[ index ];
+	if ( !op1 || !op2 ) {
+		return;
+	}
+	if ( op1.type === 'retain' && op2.type === 'retain' ) {
+		op1.length += op2.length;
+		this.operations.splice( index, 1 );
+	} else if ( op1.type === 'replace' && op2.type === 'replace' ) {
+		ve.batchSplice( op1.remove, op1.remove.length, 0, op2.remove );
+		ve.batchSplice( op1.insert, op1.insert.length, 0, op2.insert );
+		this.operations.splice( index, 1 );
+	}
+};
+
+/**
+ * Insert (in place) operations at the given offset
+ *
+ * Merges into existing operations where possible. Offset cannot be in the interior of a replace
+ * operation (i.e. the interior of its removed content).
+ *
+ * @param {number} offset The offset at which to insert
+ * @param {Object[]} operations The operations to insert
+ * @throws {Error} Offset is in the interior of a replace operation
+ */
+ve.dm.Transaction.prototype.insertOperations = function ( offset, operations ) {
+	var opIndex;
+	if ( operations.length === 0 ) {
+		return;
+	}
+	opIndex = this.trySplit( offset );
+	ve.batchSplice( this.operations, opIndex, 0, ve.copy( operations ) );
+	this.tryUnsplit( opIndex + operations.length );
+	this.tryUnsplit( opIndex );
 };
