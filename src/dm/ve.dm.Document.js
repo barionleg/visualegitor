wikimedia/VisualEditor

commit 58150e710a0e5a5a0bc373127c3611fe7ac4c85d

Use ve.dm.Change object for completeHistory
Change-Id: I3eadd4a2c53f01d28451258be1ad53dc635b555d

src/dm/ve.dm.Document.js
@@ -49,7 +49,6 @@ ve.dm.Document = function VeDmDocument( data, htmlDocument, parentDocument, inte
 	// Properties
 	this.parentDocument = parentDocument || null;
 	this.originalDocument = originalDocument || null;
-	this.completeHistory = [];
 	this.nodesByType = {};
 	this.origInternalListLength = null;
 
@@ -64,9 +63,18 @@ ve.dm.Document = function VeDmDocument( data, htmlDocument, parentDocument, inte
 		);
 	}
 	this.store = this.data.getStore();
+	this.completeHistory = new ve.dm.Change( 0, [], [], {} );
+	// Use the store by reference inside the completeHistory
+	this.completeHistory.store = this.store;
+	if ( this.store.getLength() > 0 ) {
+		// Push an identity transaction and the initial store
+		this.completeHistory.transactions.push( new ve.dm.Transaction( [ {
+			type: 'retain',
+			length: this.data.data.length
+		} ] ) );
+		this.completeHistory.storeLengthAtTransaction.push( this.store.getLength() );
+	}
 	this.htmlDocument = htmlDocument || ve.createDocumentFromHtml( '' );
-
-	this.storeLengthAtHistoryLength = [ this.store.getLength() ];
 };
 
 /* Inheritance */
@@ -325,8 +333,7 @@ ve.dm.Document.prototype.commit = function ( transaction, isStaging ) {
 	}
 	this.emit( 'precommit', transaction );
 	new ve.dm.TransactionProcessor( this, transaction, isStaging ).process();
-	this.completeHistory.push( transaction );
-	this.storeLengthAtHistoryLength[ this.completeHistory.length ] = this.store.getLength();
+	this.completeHistory.pushTransaction( transaction );
 	this.emit( 'transact', transaction );
 };
 
@@ -1670,22 +1677,22 @@ ve.dm.Document.prototype.findText = function ( query, options ) {
 };
 
 /**
- * Get the length of the complete history stack. This is also the current pointer.
+ * Get the length of the complete history. This is also the current pointer.
  *
  * @return {number} Length of the complete history stack
  */
 ve.dm.Document.prototype.getCompleteHistoryLength = function () {
-	return this.completeHistory.length;
+	return this.completeHistory.getLength();
 };
 
 /**
- * Get all the items in the complete history stack since a specified pointer.
+ * Get all the transactions in the complete history since a specified pointer.
  *
  * @param {number} start Pointer from where to start the slice
- * @return {ve.dm.Transaction[]} Array of transaction objects with undo flag
+ * @return {ve.dm.Transaction[]} Array of transaction objects
  */
 ve.dm.Document.prototype.getCompleteHistorySince = function ( start ) {
-	return this.completeHistory.slice( start );
+	return this.completeHistory.transactions.slice( start );
 };
 
 /**
@@ -1695,18 +1702,7 @@ ve.dm.Document.prototype.getCompleteHistorySince = function ( start ) {
  * @return {ve.dm.Change} Single change containing transactions since pointer
  */
 ve.dm.Document.prototype.getChangeSince = function ( start ) {
-	var t, len, transaction,
-		transactions = [],
-		stores = [];
-	for ( t = start, len = this.completeHistory.length; t < len; t++ ) {
-		transaction = this.completeHistory[ t ];
-		transactions.push( transaction );
-		stores.push( this.store.slice(
-			this.storeLengthAtHistoryLength[ t ],
-			this.storeLengthAtHistoryLength[ t + 1 ]
-		) );
-	}
-	return new ve.dm.Change( start, transactions, stores, {} );
+	return this.completeHistory.mostRecent( start );
 };
 
 /**

src/dm/ve.dm.Change.js
@@ -790,6 +790,16 @@ ve.dm.Change.prototype.concat = function ( other ) {
 };
 
 /**
+ * Push a transaction, after having pushed to the hash value store if it needs to grow
+ *
+ * @param {ve.dm.Transaction} transaction The transaction
+ */
+ve.dm.Change.prototype.pushTransaction = function ( transaction ) {
+	this.transactions.push( transaction );
+	this.storeLengthAtTransaction.push( this.store.getLength() );
+};
+
+/**
  * Push another change onto this change
  *
  * @param {ve.dm.Change} other Change that starts immediately after this
@@ -881,13 +891,13 @@ ve.dm.Change.prototype.applyTo = function ( surface ) {
  */
 ve.dm.Change.prototype.unapplyTo = function ( surface ) {
 	var doc = surface.documentModel,
-		historyLength = doc.completeHistory.length - this.getLength();
+		historyLength = doc.completeHistory.getLength() - this.getLength();
 	this.transactions.slice().reverse().forEach( function ( tx ) {
 		surface.change( tx.reversed() );
 	} );
-	doc.completeHistory.length = historyLength;
-	doc.storeLengthAtHistoryLength.length = historyLength + 1;
-	doc.store.truncate( doc.storeLengthAtHistoryLength[ historyLength ] );
+	doc.completeHistory.transactions.length = historyLength;
+	doc.completeHistory.storeLengthAtTransaction.length = historyLength;
+	doc.store.truncate( doc.completeHistory.storeLengthAtTransaction[ historyLength - 1 ] );
 };
 
 /**
@@ -897,16 +907,7 @@ ve.dm.Change.prototype.unapplyTo = function ( surface ) {
  * @throws {Error} If this change does not start at the top of the history
  */
 ve.dm.Change.prototype.addToHistory = function ( documentModel ) {
-	if ( this.start !== documentModel.completeHistory.length ) {
-		throw new Error( 'this starts at ' + this.start +
-			' but history ends at ' + documentModel.completeHistory.length );
-	}
-	// FIXME this code should probably be in dm.Document
-	this.getStores().forEach( function ( store ) {
-		documentModel.store.merge( store );
-	} );
-	ve.batchPush( documentModel.completeHistory, this.transactions );
-	documentModel.storeLengthAtHistoryLength[ documentModel.completeHistory.length ] = documentModel.store.getLength();
+	documentModel.completeHistory.push( this );
 };
 
 /**
@@ -916,13 +917,13 @@ ve.dm.Change.prototype.addToHistory = function ( documentModel ) {
  * @throws {Error} If this change does not end at the top of the history
  */
 ve.dm.Change.prototype.removeFromHistory = function ( doc ) {
-	if ( this.start + this.getLength() !== doc.completeHistory.length ) {
+	if ( this.start + this.getLength() !== doc.completeHistory.getLength() ) {
 		throw new Error( 'this ends at ' + ( this.start + this.getLength() ) +
-			' but history ends at ' + doc.completeHistory.length );
+			' but history ends at ' + doc.completeHistory.getLength() );
 	}
-	doc.completeHistory.length -= this.transactions.length;
-	doc.storeLengthAtHistoryLength.length -= this.transactions.length;
-	doc.store.truncate( doc.storeLengthAtHistoryLength[ doc.completeHistory.length ] );
+	doc.completeHistory.transactions.length -= this.transactions.length;
+	doc.completeHistory.storeLengthAtTransaction.length -= this.transactions.length;
+	doc.store.truncate( doc.completeHistory.storeLengthAtTransaction[ doc.completeHistory.getLength() - 1 ] );
 };
 
 /**

src/dm/ve.dm.Surface.js
@@ -44,7 +44,7 @@ ve.dm.Surface = function VeDmSurface( doc, config ) {
 	this.queueingContextChanges = false;
 	this.contextChangeQueued = false;
 	this.authorId = null;
-	this.lastStoredChange = 0;
+	this.lastStoredChange = doc.getCompleteHistoryLength();
 	this.autosaveFailed = false;
 
 	// Events

tests/dm/ve.dm.Change.test.js
@@ -565,11 +565,11 @@ QUnit.test( 'Same-offset typing', function ( assert ) {
 		] ) ),
 		emptyStore = new ve.dm.HashValueStore(),
 		doc = surface.documentModel,
+		saved = doc.completeHistory.serialize(),
 		clear = function () {
-			surface.change( doc.completeHistory.map( function ( tx ) {
-				return tx.reversed();
-			} ).reverse() );
-			doc.completeHistory.length = 0;
+			doc.getChangeSince( 0 ).reversed().applyTo( surface );
+			doc.completeHistory = ve.dm.Change.static.deserialize( saved, doc );
+			doc.store = doc.completeHistory.store;
 		},
 		TxInsert = ve.dm.TransactionBuilder.static.newFromInsertion;
 

tests/dm/ve.dm.RebaseServer.test.js
@@ -71,7 +71,7 @@ QUnit.test( 'Rebase', assert => ve.spawn( function* () {
 					// Client 2 receives abc and rebases over it
 					[ '2', 'receive' ],
 					[ '2', 'assert', function ( assert, client ) {
-						assert.ok( Array.isArray( client.doc.storeLengthAtHistoryLength ), 'storeLengthAtHistoryLength array not clobbered by rebase' );
+						assert.ok( Array.isArray( client.doc.completeHistory.storeLengthAtTransaction ), 'storeLengthAtTransaction array not clobbered by rebase' );
 					} ],
 					[ '2', 'assertHist', 'abc/AB?/CD!' ],
 					// Client 2 receives confirmation of AB

barionleg/visualegitor

commit 099a4e55d8729e37ca0508cede5b5c7e86cee5ab

rebaser: Prevent no-op changes from being sent
If you connected two clients to the same document, they would send each
other no-op selection changes 4 times per second, forever. This is a
regression from 58150e710a0, which made ve.dm.Document#completeHistory a
ve.dm.Change:

* Because completeHistory is now a Change, it has a .selections object.
  This object is initially empty and it superficially appears that
  nothing adds to it.
* But ve.dm.Change#addToHistory directly pushes a Change into
  completeHistory, overwriting its .selections object. When called by
  the rebaser, this adds the current user's selection.
* ve.dm.Document#getChangeSince now returns a slice of this change with
  that selection
* ve.dm.SurfaceSynchronizer#getChangeSince knows not to add the current
  selection because it hasn't changed, but also doesn't strip the
  selection in the Change it received from ve.dm.Document#getChangeSince
* ve.dm.RebaseClient#submitChange assumes that any change that has a
  non-empty .selections object is not a no-op. Every change now has a
  selection, so no-op detection breaks completely.
* Once the no-op change arrives at the other client, its onNewChange
  handler calls submitChangeThrottled under the assumption that,
  if nothing changed, no change will be submitted. But since the same
  breakage happens on that side, another no-op change is triggered.
* The two clients keep triggering each other to submit no-op changes
  forever

Change-Id: Ie8e4ca8771c7e6f93ac44c12977344a13f44a1a5

src/dm/ve.dm.Document.js
@@ -1717,7 +1717,10 @@ ve.dm.Document.prototype.getCompleteHistorySince = function ( start ) {
  * @return {ve.dm.Change} Single change containing transactions since pointer
  */
 ve.dm.Document.prototype.getChangeSince = function ( start ) {
-	return this.completeHistory.mostRecent( start );
+	var change = this.completeHistory.mostRecent( start );
+	// Remove any selections that might have been added by e.g. ve.dm.Change#addToHistory
+	change.selections = {};
+	return change;
 };
 
 /**