694fe7c3016e95f7dd3270834343a3c1a2ac1d05
Ed Sanders <esanders@wikimedia.org>
2018-07-21 17:39:17 +0200

    Add CheckList type
    Change-Id: I5cbbcaa121e2f942e926bbb19997adba09682cdc

---
+++ src/dm/nodes/ve.dm.CheckListNode.js
@@ -0,0 +1,52 @@
+/*!
+ * VisualEditor DataModel CheckListNode class.
+ *
+ * @copyright 2011-2018 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * DataModel list node.
+ *
+ * @class
+ * @extends ve.dm.BranchNode
+ *
+ * @constructor
+ * @param {Object} [element] Reference to element in linear model
+ * @param {ve.dm.Node[]} [children]
+ */
+ve.dm.CheckListNode = function VeDmCheckListNode() {
+	// Parent constructor
+	ve.dm.CheckListNode.super.apply( this, arguments );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.dm.CheckListNode, ve.dm.BranchNode );
+
+/* Static Properties */
+
+ve.dm.CheckListNode.static.name = 'checkList';
+
+ve.dm.CheckListNode.static.childNodeTypes = [ 'checkListItem' ];
+
+ve.dm.CheckListNode.static.matchTagNames = [ 'ul' ];
+
+ve.dm.CheckListNode.static.matchRdfaTypes = [ 've:checkList' ];
+
+ve.dm.CheckListNode.static.isDiffedAsList = true;
+
+ve.dm.CheckListNode.static.toDomElements = function ( dataElement, doc ) {
+	var list = doc.createElement( 'ul' );
+	list.setAttribute( 'rel', 've:checkList' );
+	return [ list ];
+};
+
+/* Methods */
+
+// ve.dm.CheckListNode.prototype.canHaveSlugAfter = function () {
+// 	return false;
+// };
+
+/* Registration */
+
+ve.dm.modelRegistry.register( ve.dm.CheckListNode );
