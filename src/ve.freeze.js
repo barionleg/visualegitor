d4b9769a09040a097fc2edc63692f47c58845ed2
Ed Sanders <esanders@wikimedia.org>
2018-09-27 13:23:22 +0100

    Add ve.deepFreeze for detecting inadvertent data modification during testing
    Change-Id: I2cc2e6afa654e81f8e565775e4197c4d23442cf6

---
+++ src/ve.freeze.js
@@ -0,0 +1,56 @@
+/*!
+ * VisualEditor Object freeze utilities.
+ *
+ * @copyright 2011-2018 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/* global Set */
+
+( function () {
+	var freezeProxyHandler = {
+		set: function ( obj, name ) {
+			throw new Error( 'Object is frozen, can\'t set property: ' + name );
+		}
+	};
+
+	if ( !window.Proxy || !window.Set ) {
+		return;
+	}
+	/**
+	 * Deep freeze an object, making it immutable
+	 *
+	 * @param {Object} object Object to freeze
+	 * @param {boolean} [onlyProperties] Only freeze properties (or array items)
+	 * @param {Set} [seen] Set of already-seen objects (for internal, recursive use)
+	 * @return {Object} Immutable deep copy of the original object
+	 */
+	ve.deepFreeze = function ( object, onlyProperties, seen ) {
+		var name, value;
+
+		if ( !seen ) {
+			seen = new Set();
+			seen.add( object );
+		}
+		for ( name in object ) {
+			if ( Object.prototype.hasOwnProperty.call( object, name ) ) {
+				value = object[ name ];
+				if (
+					// Truth check so we don't try to freeze null
+					value &&
+					typeof value === 'object' &&
+					!seen.has( value ) &&
+					!Object.isFrozen( value )
+				) {
+					seen.add( value );
+					object[ name ] = ve.deepFreeze( value, false, seen );
+				}
+			}
+		}
+
+		if ( !onlyProperties ) {
+			object = new window.Proxy( object, freezeProxyHandler );
+			Object.freeze( object );
+		}
+		return object;
+	};
+}() );
