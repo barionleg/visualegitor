4a5111ae94ae00cae7eadcf4491259fc92dd2ee7
Ed Sanders <esanders@wikimedia.org>
2018-10-22 13:10:29 +0100

    Fix test in Chrome 70
    Chrome 70+ appears to behave like Firefox now, stripping
    the excess linebreaks.
    
    Bug: T207654
    Bug: T258737
    Change-Id: Ie2f4e3f3f6f09731bf73535a3e823aeec4496104
    (cherry picked from commit f6af901dcd0dadd4ca5c334dc5c5ebda2a683e14)

--- tests/ce/ve.ce.Surface.test.js
+++ tests/ce/ve.ce.Surface.test.js
@@ -1476,7 +1476,7 @@ QUnit.test( 'onCopy', function ( assert ) {
 		];
 
 	function testRunner( doc, rangeOrSelection, expectedData, expectedOriginalRange, expectedBalancedRange, expectedHtml, expectedText, noClipboardData, msg ) {
-		var slice, isClipboardDataFormatsSupported, $expected, clipboardKey,
+		var slice, isClipboardDataFormatsSupported, $expected, clipboardKey, profile
 			testEvent = new ve.test.utils.TestEvent(),
 			clipboardData = testEvent.originalEvent.clipboardData,
 			view = ve.test.utils.createSurfaceViewFromDocument( doc || ve.dm.example.createExampleDocument() ),
@@ -1514,7 +1514,8 @@ QUnit.test( 'onCopy', function ( assert ) {
 			);
 		}
 		if ( expectedText ) {
-			if ( $.client.profile().layout === 'gecko' ) {
+			profile = $.client.profile();
+			if ( profile.layout === 'gecko' || ( profile.name === 'chrome' && profile.versionNumber >= 70 ) ) {
 				expectedText = expectedText.trim();
 			}
 			assert.strictEqual( clipboardData.getData( 'text/plain' ), expectedText, msg + ': text' );
