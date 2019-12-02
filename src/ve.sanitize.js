6307892a0d3d0897f2ae4f3e5dcc047c832acc26
David Chan <david@troi.org>
2019-12-02 14:45:04 +0800

    WIP: Sanitize HTML on paste
    Bug: T239209
    Change-Id: I1f99458fd2c4f6b2460dfe7a93b330ddee4400b6

--- src/ve.sanitize.js
+++ src/ve.sanitize.js
@@ -10,22 +10,33 @@
  * Parse and sanitize an HTML string, making user HTML safe to load on the page
  *
  * @param {string} html HTML
- * @return {NodeList} Node list
+ * @param {boolean} returnDocument If true, return whole document; else return node list
+ * @return {NodeList|HTMLDocument} Node list or HTML document
  */
-ve.sanitizeHtml = function ( html ) {
+ve.sanitizeHtml = function ( html, returnDocument ) {
 	// TODO: Move MW-specific rules to ve-mw
-	var addTags = [ 'figure-inline' ],
+	var options,
+		addTags = [ 'figure-inline' ],
 		addAttrs = [
 			'srcset',
 			// RDFa
 			'about', 'rel', 'resource', 'property', 'content', 'datatype', 'typeof'
 		];
-	return DOMPurify.sanitize( html, {
+	options = {
 		ADD_TAGS: addTags,
 		ADD_ATTR: addAttrs,
 		ADD_URI_SAFE_ATTR: addAttrs,
-		FORBID_TAGS: [ 'style' ],
-		FORCE_BODY: true,
-		RETURN_DOM_FRAGMENT: true
-	} ).childNodes;
+		FORBID_TAGS: [ 'style' ]
+	};
+	if ( !returnDocument ) {
+		options.FORCE_BODY = true;
+		options.RETURN_DOM_FRAGMENT = true;
+		return DOMPurify.sanitize( html, options ).childNodes;
+	}
+	options.RETURN_DOM = true;
+	return DOMPurify.sanitize( html, options ).ownerDocument;
+};
+
+ve.sanitizeHtmlToDocument = function ( html ) {
+	return ve.sanitizeHtml( html, true );
 };
