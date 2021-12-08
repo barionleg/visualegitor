84374506f1fed4ae16c87255569ffe071ca8bd36
David Lynch <dlynch@wikimedia.org>
2021-12-07 20:57:12 -0600

    [WIP] Fuzzy command bar
    Change-Id: If79cd0e9b1bdfe17dba631aac03856bd78012a01

---
+++ src/ui/actions/ve.ui.HelpCompletionAction.js
@@ -0,0 +1,153 @@
+/*!
+ * VisualEditor UserInterface ve.ui.HelpCompletionAction class.
+ *
+ * @copyright 2011-2021 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+( function () {
+	/**
+	 * HelpCompletionAction action.
+	 *
+	 * Controls autocompletion of anything from the help panel
+	 *
+	 * @class
+	 * @extends ve.ui.CompletionAction
+	 * @constructor
+	 * @param {ve.ui.Surface} surface Surface to act on
+	 */
+	ve.ui.HelpCompletionAction = function ( surface ) {
+		// Parent constructor
+		ve.ui.HelpCompletionAction.super.call( this, surface );
+	};
+
+	/* Inheritance */
+
+	OO.inheritClass( ve.ui.HelpCompletionAction, ve.ui.CompletionAction );
+
+	/* Static Properties */
+
+	ve.ui.HelpCompletionAction.static.name = 'HelpCompletion';
+
+	ve.ui.HelpCompletionAction.static.alwaysIncludeInput = false;
+
+	ve.ui.HelpCompletionAction.static.methods = OO.copy( ve.ui.HelpCompletionAction.static.methods );
+	ve.ui.HelpCompletionAction.static.methods.push( 'insertAndOpen' );
+
+	ve.ui.HelpCompletionAction.static.defaultLimit = 99;
+
+	/* Methods */
+
+	var sequence;
+
+	ve.ui.HelpCompletionAction.prototype.insertAndOpen = function () {
+		var inserted = false,
+			surfaceModel = this.surface.getModel(),
+			fragment = surfaceModel.getFragment();
+
+		// This is opening a window in a slightly weird way, so the normal logging
+		// doesn't catch it. This assumes that the only way to get here is from
+		// the tool. If we add other paths, we'd need to change the logging.
+		ve.track(
+			'activity.' + this.constructor.static.name,
+			{ action: 'window-open-from-tool' }
+		);
+
+		// Run the sequence matching logic again to check
+		// if we already have the sequence inserted at the
+		// current offset.
+		if ( fragment.getSelection().isCollapsed() ) {
+			inserted = this.surface.getView().findMatchingSequences().some( function ( item ) {
+				return item.sequence === sequence;
+			} );
+		}
+
+		if ( !inserted ) {
+			fragment.insertContent( '\\' );
+		}
+		fragment.collapseToEnd().select();
+
+		return this.open();
+	};
+
+	ve.ui.HelpCompletionAction.prototype.getToolTitle = function ( toolName ) {
+		var toolbar = ve.init.target.getToolbar();
+		var tool = toolbar.tools[ toolName ];
+		var title = '';
+		if ( tool.elementGroup instanceof OO.ui.PopupToolGroup ) {
+			title += tool.elementGroup.getTitle() + ' > ';
+		}
+		title += tool.getTitle();
+
+		return title;
+	};
+
+	ve.ui.HelpCompletionAction.prototype.getSuggestions = function ( input ) {
+		var action = this,
+			toolbar = ve.init.target.getToolbar(),
+			toolNames = Object.keys( toolbar.tools ).filter( function ( toolName ) {
+				var tool = toolbar.tools[ toolName ];
+				return tool instanceof ve.ui.Tool;
+			} ),
+			deferred = ve.createDeferred();
+
+		return deferred.resolve( this.filterSuggestionsForInput(
+			toolNames,
+			input,
+			function ( toolName ) {
+				return action.getToolTitle( toolName ).toLowerCase().indexOf( input ) !== -1;
+			}
+		) );
+	};
+
+	ve.ui.HelpCompletionAction.prototype.getMenuItemForSuggestion = function ( toolName ) {
+		var toolbar = ve.init.target.getToolbar();
+		var tool = toolbar.tools[ toolName ];
+		return new OO.ui.MenuOptionWidget( {
+			data: tool.constructor.static.getCommandName(),
+			label: this.getToolTitle( toolName ),
+			disabled: tool.isDisabled()
+		} );
+	};
+
+	ve.ui.HelpCompletionAction.prototype.getHeaderLabel = function () {
+		return ve.msg( 'visualeditor-dialog-command-help-title' );
+	};
+
+	ve.ui.HelpCompletionAction.prototype.onChoose = function ( item, range ) {
+		// We're completely ignoring the idea that we should be "inserting" anything...
+		// Instead, we run the command that was chosen.
+
+		var fragment = this.surface.getModel().getLinearFragment( range, true );
+		fragment.removeContent();
+		fragment.collapseToEnd();
+
+		this.surface.executeCommand( item.getData() );
+
+		return true;
+	};
+
+	ve.ui.HelpCompletionAction.prototype.shouldAbandon = function ( input ) {
+		// TODO: need to consider whether pending loads from server are happening here
+		return ve.ui.HelpCompletionAction.super.prototype.shouldAbandon.apply( this, arguments ) && input.split( /\s+/ ).length > 2;
+	};
+
+	/* Registration */
+
+	ve.ui.actionFactory.register( ve.ui.HelpCompletionAction );
+
+	var openCommand = new ve.ui.Command(
+		'openHelpCompletions', ve.ui.HelpCompletionAction.static.name, 'open',
+		{ supportedSelections: [ 'linear' ] }
+	);
+	var insertAndOpenCommand = new ve.ui.Command(
+		'insertAndOpenHelpCompletions', ve.ui.HelpCompletionAction.static.name, 'insertAndOpen',
+		{ supportedSelections: [ 'linear' ] }
+	);
+	sequence = new ve.ui.Sequence( 'autocompleteHelpCommands', 'openHelpCompletions', '\\', 0 );
+	ve.ui.commandRegistry.register( openCommand );
+	ve.ui.commandRegistry.register( insertAndOpenCommand );
+	// ve.ui.wikitextCommandRegistry.register( openCommand );
+	// ve.ui.wikitextCommandRegistry.register( insertAndOpenCommand );
+	ve.ui.sequenceRegistry.register( sequence );
+	// ve.ui.wikitextSequenceRegistry.register( sequence );
+}() );
