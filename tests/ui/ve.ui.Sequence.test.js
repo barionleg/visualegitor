a90f7f231ee8049ac3073ad91e0aa4fbea497d5f
Ed Sanders <esanders@wikimedia.org>
2021-12-16 16:37:13 +0000

    Always convert to wrapper paragraph inside lists
    We do the same thing in table cells, but because lists are created
    from existing paragraphs, this is a bit more complex.
    
    Change-Id: Ic413b0237e043050e620ebcf86c9b92790695621

--- tests/ui/ve.ui.Sequence.test.js
+++ tests/ui/ve.ui.Sequence.test.js
@@ -17,7 +17,7 @@ QUnit.test( 'findAndExecuteSequences', function ( assert ) {
 				expectedData: [
 					{ type: 'list', attributes: { style: 'bullet' } },
 					{ type: 'listItem' },
-					{ type: 'paragraph' },
+					{ type: 'paragraph', internal: { generated: 'wrapper' } },
 					{ type: '/paragraph' },
 					{ type: '/listItem' },
 					{ type: '/list' }
@@ -30,7 +30,7 @@ QUnit.test( 'findAndExecuteSequences', function ( assert ) {
 				expectedData: [
 					{ type: 'list', attributes: { style: 'number' } },
 					{ type: 'listItem' },
-					{ type: 'paragraph' },
+					{ type: 'paragraph', internal: { generated: 'wrapper' } },
 					{ type: '/paragraph' },
 					{ type: '/listItem' },
 					{ type: '/list' }
