3174cc3c8400ffcf1f09448f2cc61effe2ee1746
Ed Sanders <esanders@wikimedia.org>
2021-12-24 14:11:26 +0000

    Table diff: Convert column/row remove-inserts to changes
    Change-Id: I99cd44635d1ad435bc4a53d6f32f6676393eec01

--- tests/ui/ve.ui.DiffElement.test.js
+++ tests/ui/ve.ui.DiffElement.test.js
@@ -588,6 +588,49 @@ QUnit.test( 'Diffing', function ( assert ) {
 					'</div>'
 			},
 			{
+				msg: 'Change table columns',
+				oldDoc:
+					'<table>' +
+						'<tr><td>A</td><td>Foo</td><td>Baz</td></tr>' +
+						'<tr><td>B</td><td>Bar</td><td>Qux</td></tr>' +
+					'</table>',
+				newDoc:
+					'<table>' +
+						'<tr><td>A</td><td>Foo 1</td><td>Baz 1</td></tr>' +
+						'<tr><td>B</td><td>Bar 1</td><td>Qux 1</td></tr>' +
+					'</table>',
+				expected:
+					'<div class="ve-ui-diffElement-doc-child-change">' +
+						'<table><tbody>' +
+							'<tr><td data-diff-action="none">A</td><td>Foo<ins data-diff-action="insert"> 1</ins></td><td>Baz<ins data-diff-action="insert"> 1</ins></td></tr>' +
+							'<tr><td data-diff-action="none">B</td><td>Bar<ins data-diff-action="insert"> 1</ins></td><td>Qux<ins data-diff-action="insert"> 1</ins></td></tr>' +
+						'</tbody></table>' +
+					'</div>'
+			},
+			{
+				msg: 'Change table rows',
+				oldDoc:
+					'<table>' +
+						'<tr><td>A</td><td>B</td></tr>' +
+						'<tr><td>Foo</td><td>Bar</td></tr>' +
+						'<tr><td>Baz</td><td>Qux</td></tr>' +
+					'</table>',
+				newDoc:
+					'<table>' +
+						'<tr><td>A</td><td>B</td></tr>' +
+						'<tr><td>Foo 1</td><td>Bar 1</td></tr>' +
+						'<tr><td>Baz 1</td><td>Qux 1</td></tr>' +
+					'</table>',
+				expected:
+					'<div class="ve-ui-diffElement-doc-child-change">' +
+						'<table><tbody>' +
+							'<tr><td data-diff-action="none">A</td><td data-diff-action="none">B</td></tr>' +
+							'<tr><td>Foo<ins data-diff-action="insert"> 1</ins></td><td>Bar<ins data-diff-action="insert"> 1</ins></td></tr>' +
+							'<tr><td>Baz<ins data-diff-action="insert"> 1</ins></td><td>Qux<ins data-diff-action="insert"> 1</ins></td></tr>' +
+						'</tbody></table>' +
+					'</div>'
+			},
+			{
 				msg: 'Table row removed and cells edited',
 				oldDoc: '<table>' +
 						'<tr><td>A</td><td>B</td><td>C</td></tr>' +
@@ -716,10 +759,34 @@ QUnit.test( 'Diffing', function ( assert ) {
 				expected:
 					'<div class="ve-ui-diffElement-doc-child-change">' +
 						'<table><tbody>' +
-						'<tr><td rowspan="2" data-diff-action="insert">A</td><td data-diff-action="remove">A</td><td data-diff-action="none">B</td></tr>' +
-						'<tr><td data-diff-action="remove">C</td><td data-diff-action="none">D</td></tr>' +
+						'<tr><td rowspan="2" data-diff-id="0"><p data-diff-action="none">A</p></td><td data-diff-action="none">B</td></tr>' +
+						'<tr><td data-diff-action="none">D</td></tr>' +
 						'</tbody></table>' +
-					'</div>'
+					'</div>',
+				expectedDescriptions: [
+					'<div>visualeditor-changedesc-set,rowspan,<ins>2</ins></div>'
+				]
+			},
+			{
+				msg: 'Vertical merge and modify',
+				oldDoc: '<table>' +
+						'<tr><td>A</td><td>B</td></tr>' +
+						'<tr><td>C</td><td>D</td></tr>' +
+					'</table>',
+				newDoc: '<table>' +
+						'<tr><td rowspan="2">A</td><td>B 1</td></tr>' +
+						'<tr><td>D 1</td></tr>' +
+					'</table>',
+				expected:
+					'<div class="ve-ui-diffElement-doc-child-change">' +
+						'<table><tbody>' +
+						'<tr><td rowspan="2" data-diff-id="0"><p data-diff-action="none">A</p></td><td>B<ins data-diff-action="insert"> 1</ins></td></tr>' +
+						'<tr><td>D<ins data-diff-action="insert"> 1</ins></td></tr>' +
+						'</tbody></table>' +
+					'</div>',
+				expectedDescriptions: [
+					'<div>visualeditor-changedesc-set,rowspan,<ins>2</ins></div>'
+				]
 			},
 			{
 				msg: 'Sparse table insertion',
@@ -1346,7 +1413,7 @@ QUnit.test( 'Diffing', function ( assert ) {
 				expected:
 					'<div class="ve-ui-diffElement-doc-child-change">' +
 						'<ul><li>' +
-							'<table><tbody><tr><td data-diff-action="none">Foo</td><td data-diff-action="none">Bar</td><td data-diff-action="insert">Baz2</td><td data-diff-action="remove">Baz1</td></tr></tbody></table>' +
+							'<table><tbody><tr><td data-diff-action="none">Foo</td><td data-diff-action="none">Bar</td><td><p data-diff-action="remove">Baz1</p><p data-diff-action="insert">Baz2</p></td></tr></tbody></table>' +
 						'</li></ul>' +
 					'</div>'
 			}
