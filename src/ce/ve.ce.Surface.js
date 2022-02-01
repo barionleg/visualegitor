847e60d715e7267e9a2d6325e111ec7ef740e92f
David Chan <david@troi.org>
2022-02-01 22:32:26 +0800

    Make each ve.ce.ContentBranchNode track its own most recently rendered state
    Bug: T230325
    Change-Id: I01ba468851ed3c1be83de053c75b15f06e6cae57

--- src/ce/ve.ce.Surface.js
+++ src/ce/ve.ce.Surface.js
@@ -3709,8 +3709,10 @@ ve.ce.Surface.prototype.handleObservedChanges = function ( oldState, newState ) 
 			this.showModelSelection();
 			return;
 		} else {
-			const transaction = newState.textState.getChangeTransaction(
-				oldState.textState,
+			const prevTextState = new ve.ce.TextState( newState.node.lastRenderedContents );
+			const newTextState = new ve.ce.TextState( newState.node.$element[ 0 ] );
+			const transaction = newTextState.getChangeTransaction(
+				prevTextState,
 				dmDoc,
 				newState.node.getOffset(),
 				newState.node.unicornAnnotations
@@ -3722,6 +3724,10 @@ ve.ce.Surface.prototype.handleObservedChanges = function ( oldState, newState ) 
 				} finally {
 					this.decRenderLock();
 				}
+				newState.node.lastRenderedContents =
+						ve.ce.ContentBranchNode.static.cloneForComparison(
+							newState.node.$element[ 0 ]
+						);
 				insertedText = transaction.operations.some( ( op ) => op.type === 'replace' && op.insert.length );
 				removedText = transaction.operations.some( ( op ) => op.type === 'replace' && op.remove.length );
 			}
