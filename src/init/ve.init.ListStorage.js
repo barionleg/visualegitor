2ff855b46874a8ae0b67f3f5826ce0076bc44fc7
Ed Sanders <esanders@wikimedia.org>
2022-06-10 16:02:49 +0100

    [WIP] Update standalone storage classes with expiry functionality
    Mostly copied from the corresponding MW classes.
    
    Depends-On: Ifec82b5f8a1a106179d7f887c40c799a0c6200c6
    Change-Id: I2bfacfa9e29eca279552497076d309ad1308ad68

--- src/init/ve.init.ListStorage.js
+++ src/init/ve.init.ListStorage.js
@@ -20,8 +20,8 @@ ve.init.ListStorage.prototype.get = function ( key ) {
 /**
  * @inheritdoc ve.init.SafeStorage
  */
-ve.init.ListStorage.prototype.set = function ( key, value ) {
-	return this.storage.set( key, value );
+ve.init.ListStorage.prototype.set = function ( key, value, expiry ) {
+	return this.storage.set( key, value, expiry );
 };
 
 /**
@@ -41,8 +41,8 @@ ve.init.ListStorage.prototype.getObject = function ( key ) {
 /**
  * @inheritdoc ve.init.SafeStorage
  */
-ve.init.ListStorage.prototype.setObject = function ( key, value ) {
-	return this.storage.setObject( key, value );
+ve.init.ListStorage.prototype.setObject = function ( key, value, expiry ) {
+	return this.storage.setObject( key, value, expiry );
 };
 
 /**
@@ -50,14 +50,20 @@ ve.init.ListStorage.prototype.setObject = function ( key, value ) {
  *
  * @param {string} key Key of list to set value for
  * @param {string} value Value to set
+ * @param {number} expiry Number of seconds after which this list can be deleted
  * @return {boolean} The value was set
  */
-ve.init.ListStorage.prototype.appendToList = function ( key, value ) {
+ve.init.ListStorage.prototype.appendToList = function ( key, value, expiry ) {
 	var length = this.getListLength( key );
 
-	if ( this.set( key + '__' + length, value ) ) {
+	if ( this.set( key + '__' + length, value, expiry ) ) {
+		// Update expiry on previous items
+		// Saving the expiry data to storage is debounced so this will not trigger multiple writes.
+		for ( var i = 0; i < length; i++ ) {
+			this.setExpires( key + '__' + i, expiry );
+		}
 		length++;
-		return this.set( key + '__length', length.toString() );
+		return this.set( key + '__length', length.toString(), expiry );
 	}
 	return false;
 };
