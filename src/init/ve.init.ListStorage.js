53a83f5106a3fa27426372eabe09928215234a71
Ed Sanders <esanders@wikimedia.org>
2022-06-10 16:02:49 +0100

    Update standalone storage classes with expiry functionality
    Copied from the corresponding MW classes.
    
    Change-Id: I2bfacfa9e29eca279552497076d309ad1308ad68

--- src/init/ve.init.ListStorage.js
+++ src/init/ve.init.ListStorage.js
@@ -38,14 +38,20 @@ ve.init.createListStorage = function ( storage ) {
 	 *
 	 * @param {string} key Key of list to set value for
 	 * @param {string} value Value to set
+	 * @param {number} expiry Number of seconds after which this list can be deleted
 	 * @return {boolean} The value was set
 	 */
-	ListStorage.prototype.appendToList = function ( key, value ) {
+	ListStorage.prototype.appendToList = function ( key, value, expiry ) {
 		var length = this.getListLength( key );
 
-		if ( this.set( getIndexKey( key, length ), value ) ) {
+		if ( this.set( getIndexKey( key, length ), value, expiry ) ) {
+			// Update expiry on previous items
+			// Saving the expiry data to storage is debounced so this will not trigger multiple writes.
+			for ( var i = 0; i < length; i++ ) {
+				this.setExpires( getIndexKey( key, i ), expiry );
+			}
 			length++;
-			return this.set( getLengthKey( key ), length.toString() );
+			return this.set( getLengthKey( key ), length.toString(), expiry );
 		}
 		return false;
 	};
