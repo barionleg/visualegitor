fd6c86f7a5263cd1977dcd0a281a83c0274ebd2b
Ed Sanders <esanders@wikimedia.org>
2022-06-22 16:16:09 +0100

    ListStorage: Handle conflicts when using localStorage
    Change-Id: I3c266ee93e51d1e8c4ff449b41bfe88d95b22921

--- tests/init/ve.init.ListStorage.test.js
+++ tests/init/ve.init.ListStorage.test.js
@@ -60,3 +60,123 @@ QUnit.test( 'List methods', function ( assert ) {
 	assert.deepEqual( storage.getList( 'list' ), [], 'Removed list returns empty array' );
 
 } );
+
+QUnit.test( 'Conflict handling', function ( assert ) {
+	var store = {},
+		conflictableKeys = {
+			foo: true,
+			bar: true,
+			baz: true,
+			list: 'list'
+		},
+		storageA = ve.init.platform.createSessionStorage( store, true ),
+		storageB = ve.init.platform.createSessionStorage( store, true );
+
+	function getData( s ) {
+		var copy = ve.copy( s );
+		// eslint-disable-next-line no-underscore-dangle
+		delete copy.__conflictId;
+		return copy;
+	}
+
+	storageA.addConflictableKeys( conflictableKeys );
+	storageB.addConflictableKeys( conflictableKeys );
+
+	storageA.set( 'foo', 'hello' );
+	assert.deepEqual( getData( store ), { foo: 'hello' }, 'String stored in A' );
+
+	storageB.set( 'bar', 'world' );
+	assert.deepEqual( getData( store ), { bar: 'world' }, 'String stored in B overwrites store in A' );
+
+	storageA.set( 'baz', 'world!' );
+	assert.deepEqual(
+		getData( store ),
+		{ foo: 'hello', baz: 'world!' },
+		'Storage A overwrites storage B, and keeps first key set'
+	);
+
+	storageB.set( 'unmanagedKey', 'data' );
+	// Trigger conflict check with get
+	storageA.get( '_' );
+	assert.deepEqual(
+		getData( store ),
+		{ foo: 'hello', baz: 'world!', unmanagedKey: 'data' },
+		'Storage A overwrites storage B, but keeps unmanagedKey'
+	);
+
+	storageA.remove( 'foo' );
+	storageA.remove( 'bar' );
+	storageA.remove( 'baz' );
+	storageB.get( '_' );
+	assert.deepEqual(
+		getData( store ),
+		{ bar: 'world', unmanagedKey: 'data' },
+		'"bar" in B not removed by A'
+	);
+	storageA.get( '_' );
+	assert.deepEqual(
+		getData( store ),
+		{ unmanagedKey: 'data' },
+		'keys in A removed when A is restored'
+	);
+	// Cleanup
+	storageB.remove( 'bar' );
+
+	storageA.appendToList( 'list', 'one' );
+	assert.deepEqual(
+		getData( store ),
+		{
+			list__0: 'one',
+			list__length: '1',
+			unmanagedKey: 'data'
+		},
+		'Item appended to list in A'
+	);
+	storageB.appendToList( 'list', 'un' );
+	assert.deepEqual(
+		getData( store ),
+		{
+			list__0: 'un',
+			list__length: '1',
+			unmanagedKey: 'data'
+		},
+		'Item appended to list in B'
+	);
+	storageA.appendToList( 'list', 'two' );
+	storageA.appendToList( 'list', 'three' );
+	assert.deepEqual(
+		getData( store ),
+		{
+			list__0: 'one',
+			list__1: 'two',
+			list__2: 'three',
+			list__length: '3',
+			unmanagedKey: 'data'
+		},
+		'Items appended to list in A, clearing B list'
+	);
+	storageB.appendToList( 'list', 'deux' );
+	assert.deepEqual(
+		getData( store ),
+		{
+			list__0: 'un',
+			list__1: 'deux',
+			list__length: '2',
+			unmanagedKey: 'data'
+		},
+		'Items appended to B, restoring list'
+	);
+	storageA.removeList( 'list' );
+	assert.deepEqual( getData( store ), { unmanagedKey: 'data' }, 'List in A removed' );
+	storageB.get( '_' );
+	assert.deepEqual(
+		getData( store ),
+		{
+			list__0: 'un',
+			list__1: 'deux',
+			list__length: '2',
+			unmanagedKey: 'data'
+		},
+		'List in B restored by get'
+	);
+} );
