5a994161f3eb1f6420b9b9ac93a39b842f0e2d3b
Ed Sanders <esanders@wikimedia.org>
2022-06-22 16:16:09 +0100

    ListStorage: Handle conflicts when using localStorage
    Bug: T218663
    Change-Id: I3c266ee93e51d1e8c4ff449b41bfe88d95b22921

--- src/init/ve.init.ListStorage.js
+++ src/init/ve.init.ListStorage.js
@@ -1,6 +1,15 @@
 ve.init.createListStorage = function ( storage ) {
+	var conflictKey = '__conflictId';
+	var EXPIRY_PREFIX = '_EXPIRY_';
+
 	/**
 	 * Efficient append-only list storage extending a ve.init.SafeStorage instance
+	 *
+	 * ListStorage also implements conflict handling for localStorage.
+	 * Any time the storage is used and it is detected that another process has
+	 * modified the underlying data, all the managed keys are restored from an
+	 * in-memory cache. There is no merging of data, all managed keys are either
+	 * completely overwritten, or deleted if they were not originally set.
 	 *
 	 * This would be namespaced ve.init.ListStorage, but as a generated class
 	 * it is never exported.
@@ -14,6 +23,11 @@ ve.init.createListStorage = function ( storage ) {
 	function ListStorage() {
 		// Parent constructor
 		ListStorage.super.apply( this, arguments );
+
+		this.storageMayConflict = false;
+		this.conflictBackup = {};
+		this.conflictableKeys = {};
+		this.conflictId = null;
 	}
 
 	/* Inheritance */
@@ -34,6 +48,184 @@ ve.init.createListStorage = function ( storage ) {
 	/* Methods */
 
 	/**
+	 * @inheritdoc
+	 */
+	ListStorage.prototype.set = function ( key, value ) {
+		if ( key === conflictKey ) {
+			throw new Error( 'Can\'t set key ' + conflictKey + ' directly.' );
+		}
+		if ( this.storageMayConflict ) {
+			if ( this.isConflicted() ) {
+				this.overwriteFromBackup();
+			}
+			if ( Object.prototype.hasOwnProperty.call( this.conflictableKeys, key ) ) {
+				this.conflictBackup[ key ] = value;
+			}
+		}
+
+		// Parent method
+		return ListStorage.super.prototype.set.apply( this, arguments );
+	};
+
+	/**
+	 * @inheritdoc
+	 */
+	ListStorage.prototype.remove = function ( key, removingList ) {
+		if ( key === conflictKey ) {
+			throw new Error( 'Can\'t remove key ' + conflictKey + ' directly.' );
+		}
+		if ( !removingList && key.slice( -8 ) === '__length' ) {
+			// If a list length key is removed, remove the rest of the list.
+			// Callers should typically call removeList directly, but this
+			// will catch the interal call to #remove when a list length
+			// key expires.
+			return this.removeList( key.slice( 0, -8 ) );
+		}
+		if ( this.storageMayConflict ) {
+			if ( this.isConflicted() ) {
+				this.overwriteFromBackup();
+			}
+			if ( Object.prototype.hasOwnProperty.call( this.conflictableKeys, key ) ) {
+				delete this.conflictBackup[ key ];
+			}
+		}
+
+		// Parent method
+		return ListStorage.super.prototype.remove.apply( this, arguments );
+	};
+
+	/**
+	 * @inheritdoc
+	 */
+	ListStorage.prototype.get = function () {
+		if ( this.isConflicted() ) {
+			this.overwriteFromBackup();
+		}
+
+		// Parent method
+		return ListStorage.super.prototype.get.apply( this, arguments );
+	};
+
+	/**
+	 * @inheritdoc
+	 */
+	ListStorage.prototype.setExpires = function ( key ) {
+		// Parent method
+		ListStorage.super.prototype.setExpires.apply( this, arguments );
+
+		if ( this.storageMayConflict ) {
+			if ( Object.prototype.hasOwnProperty.call( this.conflictableKeys, key ) ) {
+				var expiryAbsolute = null;
+				try {
+					expiryAbsolute = this.store.getItem( EXPIRY_PREFIX + key );
+				} catch ( e ) {}
+
+				if ( expiryAbsolute ) {
+					this.conflictBackup[ EXPIRY_PREFIX + key ] = expiryAbsolute;
+				} else {
+					delete this.conflictBackup[ EXPIRY_PREFIX + key ];
+				}
+			}
+		}
+	};
+
+	/**
+	 * Check if another process has written to the shared storage, leaving
+	 * our data in a conflicted state.
+	 *
+	 * @return {boolean} Data is conflicted
+	 */
+	ListStorage.prototype.isConflicted = function () {
+		if ( !this.storageMayConflict ) {
+			return false;
+		}
+		// Read directly from store to avoid any caching used by sub-classes
+		try {
+			return this.store.getItem( conflictKey ) !== this.conflictId;
+		} catch ( e ) {
+			return false;
+		}
+	};
+
+	/**
+	 * Overwrite data in the store from our in-memory backup
+	 *
+	 * Only keys added in #addConflictableKeys are restored
+	 */
+	ListStorage.prototype.overwriteFromBackup = function () {
+		// Call parent method directly when setting conflict key
+		ListStorage.super.prototype.set.call( this, conflictKey, this.conflictId );
+
+		for ( var key in this.conflictableKeys ) {
+			var expiryKey, expiryAbsolute, expiry = null;
+			if ( this.conflictableKeys[ key ] === 'list' ) {
+				var listToRestore = ( this.conflictBackup[ key ] || [] ).slice();
+				this.removeList( key );
+
+				var length = listToRestore.length;
+				if ( length ) {
+					// The expiry value is stored on the length key
+					expiryKey = EXPIRY_PREFIX + getLengthKey( key );
+					expiryAbsolute = this.conflictBackup[ expiryKey ];
+					if ( expiryAbsolute ) {
+						expiry = expiryAbsolute - Math.floor( Date.now() / 1000 );
+					}
+
+					// Call parent methods directly when restoring
+					// As in appendToList, only set expiry on the length key
+					ListStorage.super.prototype.set.call( this, getLengthKey( key ), length.toString(), expiry );
+					for ( var i = 0; i < length; i++ ) {
+						ListStorage.super.prototype.set.call( this, getIndexKey( key, i ), listToRestore[ i ] );
+					}
+				}
+			} else {
+				if ( Object.prototype.hasOwnProperty.call( this.conflictBackup, key ) && this.conflictBackup[ key ] !== null ) {
+					expiryKey = EXPIRY_PREFIX + key;
+					expiryAbsolute = this.conflictBackup[ expiryKey ];
+					if ( expiryAbsolute ) {
+						expiry = expiryAbsolute - Math.floor( Date.now() / 1000 );
+					}
+
+					// Call parent methods directly when restoring
+					ListStorage.super.prototype.set.call( this, key, this.conflictBackup[ key ], expiry );
+				} else {
+					ListStorage.super.prototype.remove.call( this, key, this.conflictBackup[ key ] );
+				}
+			}
+		}
+	};
+
+	/**
+	 * Add keys which will need to be conflict-aware
+	 *
+	 * @param {Object} keys Object with conflict-aware keys as keys, and a
+	 *  value of `true` for regular items, and 'list' for items we manage
+	 *  as a append-only list, e.g. { 've-html': true, 've-changes': 'list' }
+	 */
+	ListStorage.prototype.addConflictableKeys = function ( keys ) {
+		ve.extendObject( this.conflictableKeys, keys );
+
+		this.storageMayConflict = true;
+
+		if ( !this.conflictId ) {
+			this.conflictId = Math.random().toString( 36 ).slice( 2 );
+			// Call parent method directly when setting conflict key
+			ListStorage.super.prototype.set.call( this, conflictKey, this.conflictId );
+		}
+
+		for ( var key in keys ) {
+			if ( Object.prototype.hasOwnProperty.call( keys, key ) ) {
+				if ( keys[ key ] === 'list' ) {
+					this.conflictBackup[ key ] = this.getList( key );
+				} else {
+					this.conflictBackup[ key ] = this.get( key );
+				}
+			}
+		}
+
+	};
+
+	/**
 	 * Append a value to a list stored in storage
 	 *
 	 * @param {string} key Key of list to set value for
@@ -42,8 +234,16 @@ ve.init.createListStorage = function ( storage ) {
 	 * @return {boolean} The value was set
 	 */
 	ListStorage.prototype.appendToList = function ( key, value, expiry ) {
-		var length = this.getListLength( key );
+		if ( this.isConflicted() ) {
+			this.overwriteFromBackup();
+		}
 
+		if ( this.conflictableKeys[ key ] === 'list' ) {
+			this.conflictBackup[ key ] = this.conflictBackup[ key ] || [];
+			this.conflictBackup[ key ].push( value );
+		}
+
+		var length = this.getListLength( key );
 		if ( this.set( getIndexKey( key, length ), value ) ) {
 			length++;
 			// Only set expiry on the length key. The rest of the list will
@@ -92,27 +292,9 @@ ve.init.createListStorage = function ( storage ) {
 		var length = this.getListLength( key );
 
 		for ( var i = 0; i < length; i++ ) {
-			// Parent method
-			ListStorage.super.prototype.remove.call( this, getIndexKey( key, i ) );
+			this.remove( getIndexKey( key, i ), true );
 		}
-		// Parent method
-		return ListStorage.super.prototype.remove.call( this, getLengthKey( key ) );
-	};
-
-	/**
-	 * @inheritdoc
-	 */
-	ListStorage.prototype.remove = function ( key ) {
-		if ( key.slice( -8 ) === '__length' ) {
-			// If a list length key is removed, remove the rest of the list.
-			// Callers should typically call removeList directly, but this
-			// will catch the interal call to #remove when a list length
-			// key expires.
-			return this.removeList( key.slice( 0, -8 ) );
-		} else {
-			// Parent method
-			return ListStorage.super.prototype.remove.call( this, key );
-		}
+		return this.remove( getLengthKey( key ), true );
 	};
 
 	return new ListStorage( storage.store );
