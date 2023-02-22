ba6026944ea7ff58a899bae3c432c10f5baedfd2
David Chan <david@troi.org>
2023-02-22 20:02:46 +0800

    WIP: Put zero-width space after inline focusable nodes
    This stops certain IMEs from reinterpreting ce=false text as part of editable
    candidate text.
    
    Bug: T330284
    Change-Id: I4278a152f236fe04cf1d4ac32a6084334197d12b

--- src/ce/ve.ce.FocusableNode.js
+++ src/ce/ve.ce.FocusableNode.js
@@ -247,6 +247,13 @@ ve.ce.FocusableNode.prototype.onFocusableSetup = function () {
 		return;
 	}
 
+	if ( this.getModel().isContent() && this.$element.text().slice( -1 ) !== '\u200B' ) {
+		// Support: Chrome (Android, Gboard)
+		// T330284: End inline FocusableNodes with zero-width space, to prevent IMEs
+		// reinterpreting transcluded text as editable candidate text when Backspacing
+		this.$element.append( this.getElementDocument().createTextNode( '\u200B' ) );
+	}
+
 	this.focusableSurface = this.root.getSurface();
 
 	// DOM changes (duplicated from constructor in case this.$element is replaced)
