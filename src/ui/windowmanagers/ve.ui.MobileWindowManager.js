0ceb9c08dfb55a3e89395556bcc62111614cdde4
Ed Sanders <esanders@wikimedia.org>
2023-05-16 17:20:24 +0100

    Allow mobile dialogs to be positioned like contexts
    Change-Id: I8bf392f3e0076670dc4a72bbe0ca4600ec49ec19

--- src/ui/windowmanagers/ve.ui.MobileWindowManager.js
+++ src/ui/windowmanagers/ve.ui.MobileWindowManager.js
@@ -32,7 +32,26 @@ OO.inheritClass( ve.ui.MobileWindowManager, ve.ui.SurfaceWindowManager );
 // Only allow 'small' and 'full' sizes, defaulting to 'full'
 ve.ui.MobileWindowManager.static.sizes = {
 	small: ve.ui.MobileWindowManager.super.static.sizes.small,
+	// 'context' mode makes the window stick to the bottom of the screen like
+	// context items. This should not be used while a virtual keyboard may be in use.
+	context: { width: '100%' },
 	full: ve.ui.MobileWindowManager.super.static.sizes.full
 };
 
 ve.ui.MobileWindowManager.static.defaultSize = 'full';
+
+/* Methods */
+
+ve.ui.MobileWindowManager.prototype.updateWindowSize = function ( win ) {
+	ve.ui.MobileWindowManager.super.prototype.updateWindowSize.apply( this, arguments );
+
+	// Bypass for non-current, and thus invisible, windows
+	if ( win !== this.currentWindow ) {
+		return;
+	}
+
+	// TODO: Fix upstream
+	this.$element.toggleClass( 'oo-ui-windowManager-size-context', win.getSize() === 'context' );
+
+	return this;
+};
