06536f5ef14bc8dcfc3ac4bf385365022226e378
Ed Sanders <esanders@wikimedia.org>
2023-05-23 17:32:39 +0100

    CompletionWidget: Add isolateInput mode
    This moves the input to a spearate widget, so the completion
    widget can be used on non-collapsed selections, e.g. when the
    HelpCompletionWidget is opened by the ctrl+shift+p trigger.
    
    Change-Id: I02f7cb7861fcf57e08bf1a9ed4f9aa4a5ea92c23

--- src/ui/actions/ve.ui.HelpCompletionAction.js
+++ src/ui/actions/ve.ui.HelpCompletionAction.js
@@ -219,7 +219,7 @@ ve.ui.commandRegistry.register( new ve.ui.Command(
 ) );
 ve.ui.commandRegistry.register( new ve.ui.Command(
 	'openHelpCompletionsTrigger', ve.ui.HelpCompletionAction.static.name, 'open',
-	{ supportedSelections: [ 'linear' ] }
+	{ supportedSelections: [ 'linear' ], args: [ true ] }
 ) );
 
 ve.ui.sequenceRegistry.register( new ve.ui.Sequence( 'autocompleteHelpCommands', 'openHelpCompletions', '\\', 0 ) );
