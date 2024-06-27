062a13d731ded0f13496a9b9f9d93ce1360675e1
Ebrahim Byagowi <ebrahim@gnu.org>
2024-06-27 21:30:04 +0330

    i18n: Use localized digits in VE's find and replace dialog
    Change-Id: I342961482ed88d0ce2be2caf76b3f7799157e197

--- src/ve.utils.js
+++ src/ve.utils.js
@@ -498,6 +498,16 @@ ve.msg = function () {
 };
 
 /**
+ * Format a number to localized form.
+ *
+ * @param {number} number
+ * @return {string} Formatted number
+ */
+ve.formatNumber = function ( number ) {
+	return number.toLocaleString();
+};
+
+/**
  * Get an HTML localized message with HTML or DOM arguments.
  *
  * @param {string} key Message key
